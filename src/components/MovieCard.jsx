fb2447c8671174caedb10c3fb7b586413473db0e
Matheuscara <matheuscara2000@gmail.com>
2020-09-11 18:21:37 -0300

    Code-Climate

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -8,12 +8,12 @@ class MovieCard extends Component {
 
     return (
       <div className="movie-card">
-          <img className="movie-card-image" src={imagePath} alt="filme" />
-          <h4 className="movie-card-title">{title}</h4>
-          <h5 className="movie-card-subtitle">{subtitle}</h5>
-          <p className="movie-card-storyline">{storyline}</p>
-          <Rating rating={rating} />
-        </div>
+        <img className="movie-card-image" src={imagePath} alt="filme" />
+        <h4 className="movie-card-title">{title}</h4>
+        <h5 className="movie-card-subtitle">{subtitle}</h5>
+        <p className="movie-card-storyline">{storyline}</p>
+        <Rating rating={rating} />
+      </div>
     );
   }
 }
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -19,6 +19,6 @@ class MovieList extends React.Component {
   }
 }
 
-MovieList.propTypes = { movies:PropTypes.arrayOf(PropTypes.object).isRequired }
+MovieList.propTypes = { movies:PropTypes.arrayOf(PropTypes.object).isRequired };
 
 export default MovieList;
