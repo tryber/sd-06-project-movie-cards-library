71625f93f7ce107254d0e8884ab8164459a361a8
FPhilipi <fabiphilipi@gmail.com>
2020-09-11 17:35:55 -0300

    CC

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,21 +1,21 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 class MovieCard extends Component {
-    render() {
-        const { imagePath, title, subtitle, storyline } = this.props.movie
+  render() {
+    const { imagePath, title, subtitle, storyline } = this.props.movie;
 
-        return (
-            <div>
-                <img src={imagePath}></img>
-                <br />
-                {title}
-                <br />
-                {subtitle}
-                <br />
-                {storyline}
-            </div>
-        )
-    }
+    return (
+      <div>
+        <img src={imagePath}></img>
+        <br />
+        {title}
+        <br />
+        {subtitle}
+        <br />
+        {storyline}
+      </div>
+    );
+  }
 }
 
-export default MovieCard
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,23 +1,23 @@
-import React, { Component } from 'react'
-import MovieCard from './MovieCard'
+import React, { Component } from 'react';
+import MovieCard from './MovieCard';
 
 class MovieList extends Component {
   render() {
     return (
       <div>
         {this.props.movies.map((element) => {
-          return <MovieCard
+          return (<MovieCard
             movie={element}
           // imagePath={element.imagePath}
           // title={element.title}
           // subtitle={element.subtitle}
           // storyline={element.storyline}
-          />
+          />);
         })
         }
       </div>
-    )
+    );
   }
 }
 
-export default MovieList
+export default MovieList;
