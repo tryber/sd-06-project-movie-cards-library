43d5d56a48936b3f934f745f019aa2148ac51779
Ana Flávia <anaflvia@gmail.com>
2020-09-11 16:47:49 -0300

    Alguns requisitos

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,29 @@
-// implement MovieCard component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Rating from './Rating'
+
+class MovieCard extends Component {
+  render() {
+    const { url, title, subtitle, synopsis } = this.props;
+
+    return(
+      <div className="movie-card">
+        <img src={url} />
+        <h4>{title}</h4>
+        <h5>{subtitle}</h5>
+        <p>{synopsis}</p>
+        <Rating />
+      </div>
+    )
+  };
+}
+
+MovieCard.propTypes = {
+  url: PropTypes.string,
+  title: PropTypes.string,
+  subtitle: PropTypes.string,
+  synopsis: PropTypes.string,
+  rating: PropTypes.number
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,17 @@
-// implement MovieList component here
+import React, { Component } from 'react';
+import MovieCard from './MovieCard';
+
+class MovieList extends Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <div className="container">
+        {movies.map((movie) => {
+          return <MovieCard key={movie.title} url={movie.imagePath} title={movie.title} subtitle={movie.subtitle} synopsis={movie.storyline} rating={movie.rating} />
+        })}
+      </div>
+    )
+  };
+}
+
+export default MovieList;
