f28d9b185a0fa0cc4bf381e1ac73b74b104a79e5
wfelipefarina <wfelipefarina@gmail.com>
2020-09-11 14:43:33 -0300

    Requisito 1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,11 @@
+import React, { Component } from 'react';
 
+class Header extends Component {
+    render() {
+        return(
+            <div>teste</div>
+        );
+    }
+}
+
+export default Header;
