6f127fe865b7e8bd073090a8a9518582ee8d6ee0
Luciano Scalfone <luciano.scalfone@gmail.com>
2020-09-11 15:04:48 -0300

    issues req 1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
 class Header extends React.Component {
-  render () {
+  render() {
     return (
-      <header className='movie-card-header'>
+      <header className="movie-card-header">
         <h1>Movie Cards Library</h1>
       </header>
     );
