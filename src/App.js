c9cde290bae5eb0b4d932a914b2cddecfb2427fd
paulo-lins <paulolins100@gmail.com>
2020-09-11 18:23:39 -0300

    App, MovieCard e MovieList

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import MovieList from './components/MovieList';
+import movies from './data.js';
+import MovieCard from './components/MovieCard';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <MovieList movies={movies} />
+        <MovieCard movies={movies} />
       </div>
     );
   }
