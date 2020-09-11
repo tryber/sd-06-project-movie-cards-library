c85d599a64b4b4fd5a9d3a3096d82d7a6db809ec
Daniel Jesus <dev.danieljs@gmail.com>
2020-09-11 16:56:22 -0300

    Rendering text: h1 Movie Cards Library in Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,14 @@
-// implement Header component here
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    const header = (
+      <header>
+        <h1>Movie Cards Library</h1>
+      </header>
+    );
+    return header;
+  }
+}
+
+export default Header;
