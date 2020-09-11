886b4a57a7170613cfd08c3771fff727f10c63c8
Dilenio Enderle <dilenio@gmail.com>
2020-09-11 17:13:05 -0300

    Correções Code Climate 3

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -5,16 +5,21 @@ class MovieList extends Component {
   render() {
     const { movies } = this.props;
     return (
-      movies.map(movie =>
+      movies.map((movie) =>
         <MovieCard
           key={movie.title}
           title={movie.title}
           subtitle={movie.subtitle}
-          imagePath={movie.imagePath}
-        />
+          imagePath={movie.imagePath} />
       )
     );
   }
+}
+
+MovieList.propTypes = {
+  title: propTypes.string,
+  subtitle: propTypes.string,
+  imagePath: propTypes.string
 }
 
 export default MovieList;
