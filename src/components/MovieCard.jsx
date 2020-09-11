ec6c6b8a7decb740d9018594bfb947f310241cdd
Gustavo Oliva <gustavociviloliva@gmail.com>
2020-09-11 20:18:51 -0300

    Add css nos componentes

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Rating from './Rating';
+import '../App.css';
 
 
 class MovieCard extends Component {
   render() {
     const { movie: { title, subtitle, storyline, imagePath, rating } } = this.props;
     return (
-      <div>
-        <h4>{title}</h4>
-        <h5>{subtitle}</h5>
-        <p>{storyline}</p>
-        <img src={imagePath} alt={title} />
-        <Rating rating={rating} />
+      <div className="movie-card">
+        <img className="movie-card-image" src={imagePath} alt={title} />
+        <div className="movie-card-body">
+          <h4 className="movie-card-title">{title}</h4>
+          <h5 className="movie-card-subtitle">{subtitle}</h5>
+          <p className="movie-card-storyline">{storyline}</p>
+        </div>
+        <div className="movie-card-rating">
+          <Rating rating={rating} />
+        </div>
       </div>
     );
   }
