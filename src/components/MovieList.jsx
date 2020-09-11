1d656acc5ea73fdcc680c24e45b0c63d11390533
Rods <rods.leite27@gmail.com>
2020-09-11 19:01:52 -0300

    Projeto Finalizado, falta apenas as props

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,16 @@
-// implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <div className="movie-list">
+        {movies.map((movie) => <MovieCard movie={movie} key={movie.title} />)}
+      </div>
+    );
+  }
+}
+
+
+export default MovieList;
