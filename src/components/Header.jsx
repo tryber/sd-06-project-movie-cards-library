b062de076c0e2beb1b9c3987f1159f2f99c00f8a
Micaeli Teixeira <luime6@gmail.com>
2020-09-11 18:12:51 -0300

    CodeClimate

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,12 +3,12 @@ import React from 'react';
 
 class Header extends React.Component {
   render() {
-    const header = 
-      <header> 
+    const header = (
+      <header className="movie-card-header">
         <h1>Movie Cards Library</h1>
       </header>
-
-      return header;
+    );
+    return header;
   }
 }
 
