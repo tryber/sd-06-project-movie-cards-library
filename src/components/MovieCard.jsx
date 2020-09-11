39d6eef90fa7f4e34fc9be59461f3f7070b0274b
Jean Machado <jeanvictor.arq@gmail.com>
2020-09-11 19:17:56 -0300

    renderizando imagens na tela

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,14 @@
-// implement MovieCard component here
+import React from 'react';
+import MovieList from './MovieList';
+
+class MovieCard extends React.Component {
+  render() {
+		return (
+			<div>
+				{this.props.movies.map(movie => <MovieList movie={movie} />)};
+			</div>
+		)
+	}
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,22 @@
-// implement MovieList component here
+import React from 'react';
+import movies from '../data';
+
+class MovieList extends React.Component {
+	render() {
+		const { title, subtitle, storyline, rating, imagePath } = this.props.movie;
+
+		return (
+			<div>
+				<div>
+					<p> {title} </p>
+					<p> {subtitle} </p>
+					<p> {storyline} </p>
+					<p> {rating} </p>
+					<img src={imagePath} />
+				</div>
+			</div>
+		);
+  }
+}
+
+export default MovieList;
