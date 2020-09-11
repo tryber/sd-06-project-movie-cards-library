731f8834600253ccb9a69d7055774f02cc46c857
Danilo <dansantosalfa@gmail.com>
2020-09-11 17:32:13 -0300

    7/8/9/10 requisito adicionados

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -5,6 +5,14 @@ import PropTypes from 'prop-types';
 class MovieCard extends Component {
   render(){
     const { title, subtitle, storyline, imagePath, rating } = this.props.movie;
+    return (
+        <div>
+          <img src={imagePath} alt=""/>
+            <h4>{title}</h4>
+            <h5>{subtitle}</h5>
+            <p>{storyline}</p>
+        </div>
+    );
   };
 };
 
