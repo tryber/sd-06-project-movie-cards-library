6e5e916320669c22f068c9995def571135f1ded5
MarioDuarte <marioduarte.engenheiro@gmail.com>
2020-09-11 20:04:28 -0300

    Ultimo requisito

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Rating extends React.Component {
   render() {
     return (
-      <div />
+      <p className="rating">{this.props.rating}</p>
     );
   }
 }
 
 export default Rating;
+
+Rating.propTypes = { rating: PropTypes.string };
