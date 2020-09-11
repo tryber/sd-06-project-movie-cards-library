369181dbcf149846ef80a2269c0564e1fdb0c677
Matheus Coutinho <matheuscavalcante88@hotmail.com>
2020-09-11 14:44:33 -0300

    initial commit

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,11 @@
-// implement MovieCard component here
+import React, { Component } from 'react'
+
+export default class MovieCard extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
