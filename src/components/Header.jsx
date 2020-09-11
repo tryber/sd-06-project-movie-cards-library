fddb372a9c9c952d4648801f6663ef5e2c46b968
Mariano Ariel Diaz Elsztain <made@poli.ufrj.br>
2020-09-11 15:35:45 -0300

    CC

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
-import React, { Component} from 'react';
+import React, { Component } from 'react';
 
 class Header extends Component {
-  render () {
+  render() {
     return (
       <header>
         <h1>Movie Cards Library</h1>
