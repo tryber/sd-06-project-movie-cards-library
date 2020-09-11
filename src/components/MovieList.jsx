078ff719c528d7329353068a0803f243c26bb566
Bruna Franciele <brunafranciele1902@gmail.com>
2020-09-11 15:58:14 -0300

    consertando cc sobre movieList e movieCard

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import movies from './data';
 import MovieCard from './MovieCard';
+
 class MovieList extends Component {
   render() {
     return (
-      <div className='movielist'>
+      <div className="movielist">
         {movies.map((movie) => (
           <MovieCard
             key={movie.title}
