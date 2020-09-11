2e1b51af26b8f1cf3d86819861358f1820f139ed
Matheuscara <matheuscara2000@gmail.com>
2020-09-11 18:16:44 -0300

    Code-Climate

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Rating extends Component {
   render() {
@@ -11,5 +12,7 @@ class Rating extends Component {
     );
   }
 }
+
+Rating.propTypes = { Rating:PropTypes.number.isRequired }
 
 export default Rating;
