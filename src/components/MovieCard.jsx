7621f4d404a2355ff27b569a784a8a07fd30a739
Anderson R <misterregisslv@gmail.com>
2020-09-11 16:25:06 -0300

    cards adicionados na tela

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,18 @@
-// implement MovieCard component here
+import React, { Component } from 'react';
+// import Rating from './Rating';
+
+class MovieCard extends Component {
+	render() {
+		const {image, title, subtitle, storyline} = this.props;
+		return(
+		<div className='movie-card'>
+			<img src={image} alt={`imagem do filme ${title}`} height='160px'/>
+			<h4>{title}</h4>
+			<h5>{subtitle}</h5>
+			<p>{storyline}</p>
+			{/* <Rating /> */}
+		</div>
+		)}
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,21 @@
-// implement MovieList component here
+import React, { Component } from 'react';
+import movies from '../data';
+import MovieCard from './MovieCard';
+
+class MovieList extends Component {
+	render() {
+		return (
+			movies.map((movieTitle) => {
+				return <MovieCard
+				key={movieTitle.title}
+				image={movieTitle.imagePath}
+				title={movieTitle.title}
+				subtitle={movieTitle.subtitle}
+				storyline={movieTitle.storyline}
+				/>
+			})
+		)
+	}
+}
+
+export default MovieList;
