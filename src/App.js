56698ba18c6996484c3d650a4950a86edae72ffd
Mariano Ariel Diaz Elsztain <made@poli.ufrj.br>
2020-09-11 15:52:06 -0300

    Items 3 and 4

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
-// import MovieCard from './components/MovieCard';
-// import MovieList from './components/MovieList';
-// import Rating from './components/Rating';
+import MovieList from './components/MovieList';
+import movies from './data';
+
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <MovieList movies={movies} />
     </div>
   );
 }
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,10 @@
-// implement MovieCard component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class MovieCard extends Component {
+  render() {
+    return <span>Insert Movie Here</span>;
+  }
+}
+
+export default MovieCard;
