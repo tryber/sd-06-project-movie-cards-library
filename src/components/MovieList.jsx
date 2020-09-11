e458edcfc4e9eca0593522069a17dbe92cb94bce
Julia Porto <juportob@gmail.com>
2020-09-11 15:29:33 -0300

    Fixing codeclimate

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -8,13 +8,13 @@ class MovieList extends Component {
     return (
       <div className="movielist">
         {movies.map((movie) => {
-          return <MovieCard
+          return (<MovieCard
             key={movie.title}
             title={movie.title}
             subtitle={movie.subtitle}
             storyline={movie.storyline}
             imagePath={movie.imagePath}
-          />;
+          />);
         })}
       </div>
     );
