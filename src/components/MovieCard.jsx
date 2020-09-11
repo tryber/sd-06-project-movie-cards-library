c04bbf054b1692c8f8b70889d4f131465ed845ad
Paulo <paulocheloni@gmail.com>
2020-09-11 19:22:45 -0300

    never commit like this

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,22 @@
-// implement MovieCard component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Rating from './Rating';
+
+class MovieCard extends Component {
+  render() {
+    const { title, subtitle, imagePath, storyline, rating } = this.props.movie;
+    return (
+      <ul>
+        <li><h4>{title}</h4></li>
+        <li><h5>{subtitle}</h5></li>
+        <li><img src={imagePath} alt={`${title} pic`} /></li>
+        <li><p>{storyline}</p></li>
+        <Rating rating={rating} />
+      </ul>
+    );
+  }
+}
+
+MovieCard.propTypes = { movie: PropTypes.object.isRequired };
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,18 @@
-// implement MovieList component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
+
+class MovieList extends Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <div>
+        {movies.map((movie) => <MovieCard movie={movie} key={movie.title} />)}
+      </div>
+    );
+  }
+}
+
+MovieList.propTypes = { movies: PropTypes.array };
+
+export default MovieList;
