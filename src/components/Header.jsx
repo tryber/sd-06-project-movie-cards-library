fe2ffc4a293f2a1e94181c1e5bb16620b9d414c1
Anderson R <misterregisslv@gmail.com>
2020-09-11 17:55:45 -0300

    codeclimate 2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,9 @@ import React, { Component } from 'react';
 class Header extends Component {
   render() {
     return (
-	    <header>
+      <header>
         <h1>Movie Cards Library</h1>
-		  </header>
+      </header>
     );
   }
 }
