5907417fa377946f8e957bbdd8f622b4e737510c
Pedro Risso <pmerchiorattor@gmail.com>
2020-09-11 15:07:19 -0300

    header added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,14 @@
-// implement Header component here
+import React, { Component } from 'react'
+import '../App.css';
+
+class Header extends Component {
+  render() {
+    return (
+      <header className="movie-card-header">
+        <h1>Movie Cards Library</h1>
+      </header>
+    )
+  }
+}
+
+export default Header;
