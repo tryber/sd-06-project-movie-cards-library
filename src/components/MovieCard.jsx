5445b5a7d37c3ca575c85e8296b24734b74536a5
kissyla <kissylamottapereira@gmail.com>
2020-09-11 17:31:28 -0300

    codeclimate 2.0)

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,19 +1,18 @@
 import React, { Component } from 'react';
+import Rating from './Rating';
 
 class MovieCard extends Component {
   render() {
-    console.log(this)
     const { title, subtitle, storyline, rating, imagePath } = this.props.props;
-    console.log(this);
     return (
-      <ul>
-        <li>{title}</li>
-        <li>{subtitle}</li>
-        <li>{storyline}</li>
-        <li>{rating}</li>
-        <li><img src={imagePath}></img></li>
-      </ul>
-    )
+      <div>
+        <h4>{title}</h4>
+        <h5>{subtitle}</h5>
+        <p>{storyline}</p>
+        <Rating props={rating} />
+        <img src={imagePath} alt={`Nome do filme ${title}`}></img>
+      </div>
+    );
   }
 };
 
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,15 @@
-// implement Rating component here
+import React, { Component } from 'react';
+
+class Rating extends Component {
+  render() {
+    {
+      const { rating } = this.props.props;
+    return (
+      <p props={rating}></p>
+    );
+    }
+    
+  }
+}
+
+export default Rating;
