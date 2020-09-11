3a02bdf2997dde58838e188749cc6470a24eb8bf
Cyrano webdev <cyrano@cyranowebdev.com>
2020-09-11 16:32:30 -0300

    corrigindo linhas 9-11 em MovieList.jsx

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -7,9 +7,7 @@ class MovieList extends React.Component {
     const { movies } = this.props.movies;
     return (
       <div className="movie-list">
-          {movies.map(movie => {
-            return (<MovieCard key={movie.title} movie={movie} />);
-          })}
+          {movies.map(movie => <MovieCard key={movie.title} movie={movie} />)}
       </div>
     );
   }
