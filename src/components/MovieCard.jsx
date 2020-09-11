50af3b95bdeae49ebb777d47ac5f6cd2802b643d
Bruna Franciele <brunafranciele1902@gmail.com>
2020-09-11 16:56:09 -0300

    consertando cc sobre movieList e movieCard

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,17 +3,16 @@ import PropTypes from 'prop-types';
 
 class MovieCard extends Component {
   render() {
-    const { title, subtitle, storyline, imagePath } = this.props;
-
+    const { title, subtitle, storyline, imagePath } = this.props.movie;
     return (
-      <div className="pokemon">
+      <div className="moviecard">
         <div>
-          <img src={imagePath} alt={title} />
-          <h4>{title}</h4>
-          <h5>{subtitle}</h5>
-          <p>{storyline}</p>
+          <img src={imagePath} alt={`Movie: ${title}`} />
         </div>
         <br />
+        <h4>{title}</h4>
+        <h5>{subtitle}</h5>
+        <p>{storyline}</p>
       </div>
     );
   }
