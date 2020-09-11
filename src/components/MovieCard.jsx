35fddf097284299ed1345183e2fce285403db3db
AlexanderFeitosa <alexander.feitosa@gmail.com>
2020-09-11 18:07:56 -0300

    Implementaçao de MovieList, MovieCard e Rating

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,28 @@
-// implement MovieCard component here
+import React, { Component } from 'react';
+import Rating from './Rating';
+import PropTypes from 'prop-types';
+
+class MovieCard extends Component {
+  render() {
+    const { title, subtitle, storyLine, imagePath, rating } = this.props;
+    return (
+      <div>
+        <img src={imagePath} alt=""></img>
+        <h4>{`${title}`}</h4>
+        <h5>{`${subtitle}`}</h5>
+        <p>{`${storyLine}`}</p>
+        <Rating rating={rating} />
+      </div>
+    );
+  }
+}
+
+MovieCard.propTypes = {
+  title: PropTypes.string,
+  subtitle: PropTypes.string,
+  storyLine: PropTypes.string,
+  imagePath: PropTypes.string,
+  rating: PropTypes.number,
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,22 @@
-// implement MovieList component here
+import React, { Component } from 'react';
+import movies from '../data';
+import MovieCard from './MovieCard';
+
+class MovieList extends Component {
+  render() {
+    return movies.map((movie) => {
+      return (
+        <MovieCard
+          key={`${movie.title}`}
+          imagePath={movie.imagePath}
+          title={movie.title}
+          subtitle={movie.subtitle}
+          storyLine={movie.storyline}
+          rating={movie.rating}
+        />
+      )
+    });
+  }
+}
+
+export default MovieList;
