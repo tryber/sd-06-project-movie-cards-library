9c724e63ab18ea5655347604ff429ec6ddfa2c03
HCaetano <hrcgeo@gmail.com>
2020-09-11 16:03:55 -0300

    Add 'MovieList' component

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,16 @@
-// implement MovieList component here
+import { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class MovieList extends Component {
+  render() {
+    const { movies } = this.props;
+
+    return movies;
+  }
+}
+
+MovieList.defaultProps = { movies: [] };
+
+MovieList.propTypes = { movies: PropTypes.arrayOf(PropTypes.objectOf(PropTypes.string)) };
+
+export default MovieList;
