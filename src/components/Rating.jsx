6f4dec51dd858feec005eef5301803e47caf6a26
Anderson <andersoni.trybe@gmail.com>
2020-09-11 18:07:46 -0300

    CodeClimate arrayOf MovieList

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -2,9 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class Rating extends Component {
-  static defaultProps = {
-    rating: null,
-  }
+  static defaultProps = { rating: null }
   render() {
     const { rating } = this.props;
     return (
