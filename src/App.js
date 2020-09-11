6e61cd33a45a1cc8ad957b0f15c30ef2dac9274f
Isabela Rosa <isabelarosa954@gmail.com>
2020-09-11 18:52:42 -0300

    adicionando css ao projeto

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import movies from './data';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App body">
       <Header />
       <MovieList movies={movies} />
     </div>
