c6e475b8ac25d4c89b5be39b910923e2703ed075
LeonardoCavachini <cavachini240@gmail.com>
2020-09-11 20:13:10 -0300

    requisito 1 ao 17

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,23 @@
+
 // implement MovieList component here
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
+
+
+class MovieList extends Component {
+    render() {
+        const {movies} = this.props;
+        return (
+            <div>
+                {movies.map(ele => <MovieCard key={ele.title} movie={ele}/>)}
+            </div>
+        )
+    }
+}
+
+MovieList.propTypes = {
+    movies: PropTypes.arrayOf.isRequired,
+}
+
+export default MovieList;
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,15 @@
 // implement Rating component here
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+class Rating extends Component {
+    render() {
+        return (
+            <span className='rating'>{this.props.rating}</span>
+        )
+    }
+}
+Rating.propTypes = {
+    rating: PropTypes.number.isRequired,
+}
+export default Rating;
