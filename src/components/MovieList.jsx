0c72f7f959982c05a163348d2f6d5b1472ca69c8
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2020-09-11 19:15:15 -0300

    Maybe final commit

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -7,17 +7,13 @@ class MovieList extends React.Component {
     return (
       <div>
         {this.props.movies.map((movie) => {
-          return (
-            <MovieCard
-              movie={movie}
-              key={movie.title}
-            />
-          );
+          const card = <MovieCard movie={movie} key={movie.title} />;
+          return (card);
         })}
       </div>
     );
   }
 }
 
-MovieList.propTypes = { movies: PropTypes.arrayOf(PropTypes.object) };
+MovieList.propTypes = { movies: PropTypes.arrayOf(PropTypes.object).isRequired };
 export default MovieList;
