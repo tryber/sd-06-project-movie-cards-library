9837e6fc5fe6dee5a6cde3e7b2375281eda17cf6
FPhilipi <fabiphilipi@gmail.com>
2020-09-11 18:46:47 -0300

    CC

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -6,16 +6,7 @@ class MovieList extends Component {
   render() {
     return (
       <div>
-        {this.props.movies.map((element) => {
-          return (<MovieCard
-            movie={element}
-          // imagePath={element.imagePath}
-          // title={element.title}
-          // subtitle={element.subtitle}
-          // storyline={element.storyline}
-          />);
-        })
-        }
+        {this.props.movies.map((element) => <MovieCard movie={element} />)}
       </div>
     );
   }
