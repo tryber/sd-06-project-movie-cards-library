b327a19c730d94885ca711bfc1d6c7718f7ac880
Ana Flávia <anaflvia@gmail.com>
2020-09-11 17:42:44 -0300

    Code climate 2

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,26 +1,26 @@
 import React, { Component } from 'react';
-import MovieCard from './MovieCard';
 import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
 
 class MovieList extends Component {
   render() {
     const { movies } = this.props;
     return (
       <div className="movie-list">
-        {movies.map((movie) => {
-          return (<MovieCard
+        {movies.map((movie) => (
+          (<MovieCard
             key={movie.title}
             url={movie.imagePath}
             title={movie.title}
             subtitle={movie.subtitle}
             synopsis={movie.storyline}
-            rating={movie.rating} />); }
+            rating={movie.rating} />) )
           )}
       </div>
     );
   }
 }
 
-MovieList.propTypes = { movies: PropTypes.any.isRequired };
+MovieList.propTypes = { movies: PropTypes.arrayOf.isRequired };
 
 export default MovieList;
