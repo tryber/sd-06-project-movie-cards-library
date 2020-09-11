ded597747787beb78309cd213b5eb9bddfb517ca
Matheus Coutinho <matheuscavalcante88@hotmail.com>
2020-09-11 16:13:37 -0300

    applying css

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -5,7 +5,7 @@ export default class Rating extends Component {
   render() {
     const { rating } = this.props;
     return (
-      <div>
+      <div className="movie-card-rating">
         <span className="rating">{rating}</span>
       </div>
     );
