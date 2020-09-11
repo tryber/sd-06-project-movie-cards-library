02cf77c5fc18981cb53c999c5ded00315438d8df
AlexanderFeitosa <alexander.feitosa@gmail.com>
2020-09-11 19:31:14 -0300

    Implementaçao de MovieList, MovieCard e Rating Codeclimate 03

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -4,14 +4,14 @@ import MovieCard from './MovieCard';
 
 class MovieList extends Component {
   render() {
-    return movies.map(movie => <MovieCard
+    return movies.map((movie) => <MovieCard
       key={`${movie.title}`}
       imagePath={movie.imagePath}
       title={movie.title}
       subtitle={movie.subtitle}
       storyLine={movie.storyline}
       rating={movie.rating}
-    /> );
+    />);
   }
 }
 
