6815c38a97fc6634aa9cd596455f33bb942f9f65
Maria Alice Guimaraes Carneiro <mguimaraes.carneiro@gmail.com>
2020-09-11 16:02:42 -0300

    2nd and 3rd Requirements 02

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class MovieList extends React.Component {
   render() {
     return (
-    <span>{this.props.movies}</span>
+      <span>{this.props.movies}</span>
     );
   }
 }
+
+MovieList.propTypes = {
+  movies: PropTypes.array
+};
 
 export default MovieList;
 /* aqui exportamos o MovieList para podermos acessa-lo no App.js
