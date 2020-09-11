fb8b04267d9d1392cdeb5e412b4728de526b9038
Danilo <dansantosalfa@gmail.com>
2020-09-11 17:33:53 -0300

    11 requisito adicionados

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Rating from './Rating';
 import PropTypes from 'prop-types';
 
 
@@ -11,6 +12,7 @@ class MovieCard extends Component {
             <h4>{title}</h4>
             <h5>{subtitle}</h5>
             <p>{storyline}</p>
+            <Rating rating={rating} />
         </div>
     );
   };
