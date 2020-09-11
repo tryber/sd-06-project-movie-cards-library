655b0b5a061dfded30ca7075e235b2d88e656fea
MarioDuarte <marioduarte.engenheiro@gmail.com>
2020-09-11 19:13:44 -0300

    Requisito 7 ao 12 cc

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,19 +1,30 @@
 import React from 'react';
-import Rating from './Rating'
+import PropTypes from 'prop-types';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
     const { title, subtitle, storyline, imagePath, rating } = this.props.movie;
     return (
-     <div>
-      <img src={imagePath} />
-      <h4>{title}</h4>
-      <h5>{subtitle}</h5>
-      <p>{storyline}</p>
-      <Rating rating={rating} />
-     </div>
+      <div>
+        <img src={imagePath} />
+        <h4>{title}</h4>
+        <h5>{subtitle}</h5>
+        <p>{storyline}</p>
+        <Rating rating={rating} />
+      </div>
     );
   }
 }
 
 export default MovieCard;
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    imagePath: PropTypes.string,
+    rating: PropTypes.string,
+  })
+};
