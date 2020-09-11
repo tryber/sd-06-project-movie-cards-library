35ec451d1c9cd7b787195738a69de35faed45fbc
Mariano Ariel Diaz Elsztain <made@poli.ufrj.br>
2020-09-11 16:07:25 -0300

    CC and item 5

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,16 +1,23 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import MovieCard from './MovieCard';
 
 class MovieList extends Component {
   render() {
     const { movies } = this.props;
-    return movies.map(movie => <MovieCard movie={movie} />);
+    return movies.map((movie) => (
+      <MovieCard
+        key={movie.title}
+        title={movie.title}
+        subtitle={movie.subtitle}
+        storyline={movie.storyline}
+        rating={movie.rating}
+        imagePath={movie.imagePath}
+      />
+    ));
   }
 }
 
-MovieList.propTypes = {
-  movies: PropTypes.arrayOf(PropTypes.object)
-}
+// MovieList.propTypes = { movies: PropTypes.arrayOf(PropTypes.object) };
 
 export default MovieList;
