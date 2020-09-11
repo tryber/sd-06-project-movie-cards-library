ed45ea60c54718bd4944b6d18a99df6a0edc585a
Marxneves <marxkhristie@gmail.com>
2020-09-11 14:13:23 -0300

    Iniciando projeto React com algumas implementaçoes iniciais

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,21 @@
-// implement MovieCard component here
+import {Component} from 'react';
+import Rating from './Rating';
+import movies from '../data';
+class MovieCard extends Component {
+  render() {
+    const {image} = this.props;
+    return <div>
+      <div>
+        <img src={image}/>
+      </div>
+      <div>
+        <h2></h2>
+        <h3></h3>
+        <p></p>
+      </div>
+      <Rating value={movies[0].rating}/>
+    </div>
+  }
+}
+
+export default MovieCard;
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,16 @@
-// implement Rating component here
+import React from 'react';
+
+class Rating extends React.Component {
+  render() {
+    return <div>
+      <div>
+        Rating
+      </div>
+      <div>
+        {this.props.value}
+      </div>
+    </div>
+  }
+}
+
+export default Rating;
