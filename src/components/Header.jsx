5ef74d483226ba43b5eff4698695e7c49b0e7742
paulo-lins <paulolins100@gmail.com>
2020-09-11 15:48:06 -0300

    Fixing Code Climate issues

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,14 @@
 // implement Header component here
 import React from 'react';
-import Proptypes from 'prop-types';
 
 class Header extends React.Component {
-    render() {
-        return (
-            <header className="movie-card-header">
-                <h1 className="page-title">Movie Cards Library</h1>
-            </header>
-        );
-    }
+  render() {
+    return (
+      <header className="movie-card-header">
+        <h1 className="page-title">Movie Cards Library</h1>
+      </header>
+    );
+  }
 }
 
 export default Header;
