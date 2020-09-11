694c0cb5b708f1fa1fc12ef7c8b9e625b2eafc4e
LeandroParisi <lepcbelisario@gmail.com>
2020-09-11 17:01:49 -0300

    Requisito 3, 4, 5: MOVIELIST

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,15 @@
-// implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render() {
+    const { title, subtitle, storyline, rating, imagePath } = this.props;
+    return(
+      <div className='movie-card'>
+        {title}
+        {subtitle}
+      </div>
+    )
+  }
+}
+
+export default MovieCard;
