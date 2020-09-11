fef8d9b4ea097d76e411651a59de1e10566c4221
Igor Leão <igorleao.eng@gmail.com>
2020-09-11 17:51:05 -0300

    add requiredes 8

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,28 @@
-// implement MovieCard component here
+import React from 'react';
+import propTypes from 'prop-types';
+
+class MovieCard extends React.Component {
+  render() {
+    const { title, subtitle, storyline, imagePath, rating } = this.props;
+
+    return (
+      <div className="movieCard">
+        <img src={imagePath} alt={title} />
+        <h4>{title}</h4>
+        <h5>{subtitle}</h5>
+        <p>{storyline}</p>
+        
+      </div>
+    );
+  }
+}
+
+MovieCard.propTypes = {
+    title: propTypes.string,
+    subtitle: propTypes.string,
+    storyline: propTypes.string,
+    imagePath: propTypes.string,
+    rating: propTypes.number
+};
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,21 @@
-// implement MovieList component here
+import React from 'react';
+import propTypes from 'prop-types';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+    return (
+       <div>
+         {movies.map((movie) => (
+          <MovieCard movie={movie} key={movie.title} />
+        ))}
+       </div>
+    );
+  }
+}
+
+MovieList.propTypes = { movies: propTypes.arrayOf(propTypes.object).isRequired };
+
+
+export default MovieList;
