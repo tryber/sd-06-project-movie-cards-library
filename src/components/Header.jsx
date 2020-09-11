ee0a0d7391108989bdb169dd5b7512b89d407198
vapatty6848 <vapatty@gmail.com>
2020-09-11 15:08:09 -0300

    testando o codeclimate

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 class Header extends React.Component {
-  render () {
-
+  render() {
+    return (
+      <div>
+        <h1> Movie Cards Library </h1>
+      </div>
+    );
   }    
 }
 export default Header;
