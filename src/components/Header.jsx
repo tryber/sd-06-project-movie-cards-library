7dd06b354b3679cf6fd74c92d430b4f0b5a78582
wfelipefarina <wfelipefarina@gmail.com>
2020-09-11 14:45:10 -0300

    Requisito 1, fixes codeclimate

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 
 class Header extends Component {
-    render() {
-        return(
-            <div>teste</div>
-        );
-    }
+  render() {
+      return (
+          <div>teste</div>
+      );
+  }
 }
 
 export default Header;
