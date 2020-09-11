3064d8da6d9e0fbf4c8413a3c48b5d1ecb2e781a
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2020-09-11 15:31:53 -0300

    Adding first and second requirement

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,22 @@
-// implement Header component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+class Header extends React.Component {
+  render() {
+
+    const headerText = 'Movie Cards Library';
+
+    return (
+      <header>
+        <h1>{headerText}</h1>
+      </header>
+    )
+  }
+}
+
+Header.propTypes = {
+  headerText: PropTypes.string,
+}
+
+export default Header;
