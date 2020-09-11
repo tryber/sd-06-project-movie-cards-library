e355fe959764c24ca9ab503f68a7ca05427969bc
Rodrigo Santana <rodrigodesantanaribeiro@gmail.com>
2020-09-11 20:27:07 -0300

    fix codeclimate

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
 class Header extends Component {
     render() {
-      return (
-        <header>
-          <h1>Movie Cards Library</h1>
-        </header>
-      );
+        return (
+            <header>
+                <h1>Movie Cards Library</h1>
+            </header>
+        );
     }
-  }
-  
-  export default Header;
-  
+}
+
+export default Header;  
