998e7cdbdf1b1e81f7e8d70620b158cdcca10c8d
Anderson R <misterregisslv@gmail.com>
2020-09-11 17:52:45 -0300

    codeclimate

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -7,14 +7,14 @@ class MovieCard extends Component {
     const { image, title, subtitle, storyline } = this.props;
     return (
       <div className="movie-card">
-			  <img src={image} alt={`imagem do filme ${title}`} height="160px" />
-			  <h4>{title}</h4>
-			  <h5>{subtitle}</h5>
-			  <p>{storyline}</p>
-			  {/* <Rating /> */}
-		  </div>
-		);
-	};
+        <img src={image} alt={`imagem do filme ${title}`} height="160px" />
+        <h4>{title}</h4>
+        <h5>{subtitle}</h5>
+        <p>{storyline}</p>
+        {/* <Rating /> */}
+      </div>
+    );
+  };
 };
 
 MovieCard.propTypes = {
