21849565e5569cbba161e238be67ff22a68f18bf
FPhilipi <fabiphilipi@gmail.com>
2020-09-11 17:52:11 -0300

    CC

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import movies from './data'
+import movies from './data';
 import Header from './components/Header';
 import MovieList from './components/MovieList';
 
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <MovieList movies={movies}/>
+      <MovieList movies={movies} />
     </div>
   );
 }
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class MovieCard extends Component {
   render() {
@@ -16,6 +17,13 @@ class MovieCard extends Component {
       </div>
     );
   }
+}
+
+MovieCard.protoType = {
+  imagePath: PropTypes.string,
+  title: PropTypes.string,
+  subtitle: PropTypes.string,
+  storyline: PropTypes.string
 }
 
 export default MovieCard;
