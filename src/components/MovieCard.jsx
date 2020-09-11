01b30846a9731d72c3d036186c75c19b683e823b
Isadora Koga <isadorakoga710@gmail.com>
2020-09-11 17:07:53 -0300

    Testando requisitos, pagina renderizando moviecard com os filmes menos a imagem

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,22 @@
-// implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render() {
+    const { title, subtitle, storyline, rating, imagePath } = this.props;
+    return (
+      <div className="movie-card">
+        <div className="movie-card-image">
+          <img src={imagePath} alt="filme" />
+        </div>
+        <div className="movie-car-body">
+          <h4 className="movie-card-title">{title}</h4>
+          <h5 className="movie-card-subtitle">{subtitle}</h5>
+          <p>{storyline}</p>
+          {/* <Rating rating={rating} /> */}
+        </div>
+      </div>
+      )
+  }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,24 @@
-// implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+import movies from '../data';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      <section className="movie-list">
+        {movies.map((movie) => {
+          return <MovieCard
+          key={movie.title}
+          title={movie.title}
+          subtitle={movie.subtitle}
+          storyline={movie.storyline}
+          rating={movie.rating}
+          image={movie.imagePath}
+          />
+      })}
+      </section>
+    )
+  }
+}
+
+export default MovieList;
