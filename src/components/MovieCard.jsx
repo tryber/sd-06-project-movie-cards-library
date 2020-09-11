1c3b8b7add1a04952a93212a8420be9507d2c98c
Matheus Coutinho <matheuscavalcante88@hotmail.com>
2020-09-11 15:23:31 -0300

    MovieCard component tests

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,11 +1,17 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Rating from './Rating';
 
 export default class MovieCard extends Component {
   render() {
+    const { title, subtitle, storyline, rating, imagePath } = this.props.movie;
     return (
       <div>
-        
+        <img src={imagePath} alt={title} />
+        <h4>{title}</h4>
+        <h5>{subtitle}</h5>
+        <p>{storyline}</p>
+        <Rating rating={rating} />
       </div>
-    )
+    );
   }
 }
