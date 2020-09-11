eea7f96a2b571d16a0f7c0591ecad6fd86159d55
Bruno Dantas <bruno.wesley77@gmail.com>
2020-09-11 18:59:36 -0300

    add header h1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,2 +1,13 @@
 // implement Header component here
-// Iniciando o projeto
+
+import React from "react";
+
+class Header extends React.Component {
+  render() {
+    return (
+      <h1>Movie Cards Library</h1>
+    )
+  }
+}
+
+export default Header;
