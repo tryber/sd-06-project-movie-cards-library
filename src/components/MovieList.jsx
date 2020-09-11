6ff348b531c5812cc7dcafcbe7af361108fbe1b7
isabellavjs <isabellavjs@gmail.com>
2020-09-11 19:15:14 -0300

    Codeclimate

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -5,6 +5,7 @@ import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
+    const { movies } = this.props;
     return (
       <div>
         {this.props.movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
