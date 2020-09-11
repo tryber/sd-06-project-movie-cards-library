5a3b6750caa4516854b679b2d2a1d403b75d1598
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2020-09-11 19:03:14 -0300

    Lets check for issues 6

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -18,14 +18,6 @@ class MovieCard extends React.Component {
   }
 }
 
-MovieCard.propTypes = {
-  movie: PropTypes.shape({
-    title: PropTypes.string,
-    subtitle: PropTypes.string,
-    storyline: PropTypes.string,
-    rating: PropTypes.number,
-    imagePath: PropTypes.string
-  })
-}
+MovieCard.propTypes = { movie: PropTypes.objectOf(PropTypes.string).isRequired };
 
 export default MovieCard;
