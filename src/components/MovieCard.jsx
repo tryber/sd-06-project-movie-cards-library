15c5657d64fae2abd7aae6e844b5023f597ee921
Daniel Menescal <danielmjales@bct.ect.ufrn.br>
2020-09-11 18:49:40 -0300

    fix code climate

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -22,12 +22,12 @@ class MovieCard extends React.Component {
 
 MovieCard.propTypes = {
   movie: PropTypes.shape({
-      title: PropTypes.string,
-      subtitle: PropTypes.string,
-      storyline: PropTypes.string,
-      imagePath: PropTypes.string,
-      rating: PropTypes.number,
-  }).isRequired
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    imagePath: PropTypes.string,
+    rating: PropTypes.number,
+  }).isRequired,
 };
 
 export default MovieCard;
