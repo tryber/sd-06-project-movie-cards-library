336279dde0a323ce92a3fd672ef69269e54f12b3
Julia Porto <juportob@gmail.com>
2020-09-11 17:57:28 -0300

    Fixing codeclimate

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,9 +4,10 @@ import PropTypes from 'prop-types';
 class MovieCard extends Component {
   render() {
     const { movie } = this.props;
+
     return (
-      <div className="moviecard">
-        <img src={movie.imagePath} alt={`Movie: ${movie.title}`} />
+      <div className="movie-card">
+        <img src={movie.imagePath} alt={movie.title} />
         <h4>{movie.title}</h4>
         <h5>{movie.subtitle}</h5>
         <p>{movie.storyline}</p>
@@ -15,11 +16,6 @@ class MovieCard extends Component {
   }
 }
 
-MovieCard.propTypes = {
-  title: PropTypes.string.isRequired,
-  subtitle: PropTypes.string.isRequired,
-  storyline: PropTypes.string.isRequired,
-  imagePath: PropTypes.string.isRequired,
-};
+MovieCard.propTypes = { movie: PropTypes.string.isRequired };
 
 export default MovieCard;
