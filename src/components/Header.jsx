6bc40b67ecf71611a9431f6d8fbf8963376a19e2
Pedro Duarte <ikbenpedro@gmail.com>
2020-09-11 17:59:05 -0300

    primeiro e segundo requisito - cc 1.0

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,11 +4,11 @@ import React from 'react';
 class Header extends React.Component {
 	render() {
 		return (
-			<header>
-				<h1>Movie Cards Library</h1>
-			</header>
-		)
- }
+		<header>
+		<h1>Movie Cards Library</h1>
+		</header>
+		);
+	}
 }
 
 export default Header;
