aaccc665435f60f5fead9240ca4e4d2e07e004f8
Julia Porto <juportob@gmail.com>
2020-09-11 18:20:20 -0300

    Adding semicolan

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 class Rating extends Component {
   render() {
-    const { rating } = this.props
+    const { rating } = this.props;
     return (
       <div className="rating">(rating)</div>
     );
