cd0c00d770ade8749906dfb803733b8b034aee6c
Tiago Berwanger <berwangertiago@gmail.com>
2020-09-11 15:22:00 -0300

    criando e renderizando os componentes

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,11 @@
 // implement MovieCard component here
+import React from 'react';
+import movies from '../data';
+
+class MovieCard extends React.Component {
+  render() {
+    return 'test';
+  }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,12 @@
 // implement MovieList component here
+import React from 'react';
+import movies from '../data';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    return 'test';
+  }
+}
+
+export default MovieList;
