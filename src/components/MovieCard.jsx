cd48bcdff2a740b30f95c5b06c0c0a286efd7fcb
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2020-09-11 19:15:41 -0300

    Add image of movie

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,8 +2,9 @@ import React, { Component } from 'react';
 
 class MovieCard extends Component {
   render() {
+    const { movie: {title, subtitle, storyline, imagePath, rating} } = this.props;
     return (
-      <img />
+      <img src={imagePath} alt={`Movie: ${title}`} />
     );
   }
 }
