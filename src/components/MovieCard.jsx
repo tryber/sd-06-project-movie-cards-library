874ec75f009675a942bf7bf7ccfc5d7e57022a24
Isabela Rosa <isabelarosa954@gmail.com>
2020-09-11 16:44:56 -0300

    adicionando proptypes ao movie card e rating

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,19 +1,28 @@
 import React from 'react';
 import Rating from './Rating';
+import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
     const { title, subtitle, storyline, imagePath, rating } = this.props.movie;
     return (
       <div>
-        <img src={imagePath} />
+        <img src={imagePath} alt={title} />
         <h4>{title}</h4>
         <h5>{subtitle}</h5>
         <p>{storyline}</p>
-        <Rating rating={rating}/>
+        <Rating rating={rating} />
       </div>
     );
   }
 }
+
+MovieCard.PropTypes = {
+  title: PropTypes.string,
+  subtitle: PropTypes.string,
+  storyline: PropTypes.string,
+  imagePath: PropTypes.any,
+  rating: PropTypes.number,
+};
 
 export default MovieCard;
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Rating extends React.Component {
   render() {
     const { rating } = this.props;
     return (
-      <div className='rating'>
+      <div className="rating">
         {rating}
       </div>
     );
   }
 }
+
+Rating.PropTypes = { rating: PropTypes.number };
 
 export default Rating;
