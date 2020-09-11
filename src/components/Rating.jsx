6a7d7c5d5f67a5c65a6eb6a1683b661931cb8248
Anderson <andersoni.trybe@gmail.com>
2020-09-11 18:16:05 -0300

    CodeCliamte again

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -2,17 +2,16 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class Rating extends Component {
-  static defaultProps = { rating: null }
+  // static defaultProps = { rating: null };
   render() {
-    const { rating } = this.props;
+    // const { rating } = this.props;
     return (
       <small className="rating">
-        {rating}
+        {this.props.rating}
       </small>
     );
   }
 }
-
-Rating.propTypes = { rating: PropTypes.number };
+// Rating.propTypes = { rating: PropTypes.number };
 
 export default Rating;
