4aac0a821be4edbc331b50a1ebc92d409719e335
Julia Porto <juportob@gmail.com>
2020-09-11 16:19:42 -0300

    Adding the right path

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-import movies from './data';
+import movies from '../data';
 import MovieCard from './MovieCard';
-
 
 class MovieList extends Component {
   render() {
