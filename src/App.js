c661ed7886c6baa75fef49f61d1deac88da9cefa
Maria Alice Guimaraes Carneiro <mguimaraes.carneiro@gmail.com>
2020-09-11 15:56:44 -0300

    2nd and 3rd Requirements

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import './App.css';
 // aqui importamos o css do app
 import Header from './components/Header';
 // aqui importamos a header criada no arquivo Header
+import MovieList from './components/MovieList';
+// aqui importamos o MovieList criada no arquivo MovieList
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <MovieList />
     </div>
   );
 }
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,14 @@
-// implement MovieList component here
+import React from 'react';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+    <span>{this.props.movies}</span>
+    );
+  }
+}
+
+export default MovieList;
+/* aqui exportamos o MovieList para podermos acessa-lo no App.js
+export "default" para exportar so um objeto, apenas um item.
+cada arquivo so pode ter um */
