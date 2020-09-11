22e1e72c8ec6611ee7d291359586c86bef28bfa3
Cássio Rubens <cassiorubensadv@gmail.com>
2020-09-11 18:48:40 -0300

    Criando componente MovieList

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import movies from '../data';
+import MovieList from './MovieList';
 
 class Header extends Component {
   render() {
@@ -7,6 +9,18 @@ class Header extends Component {
         <header>
           <h1>Movie Cards Library</h1>
         </header>
+        <div>
+          {movies.map((filme, index) => {
+            return <MovieList
+              key={index}
+              title={filme.title}
+              subtitle={filme.subtitle}
+              storyline={filme.storyline}
+              rating={filme.rating}
+              imagePath={filme.imagePath}
+            />
+          })}
+        </div>
       </div>
     );
   }
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,26 @@
-// implement MovieList component here
+import React, { Component } from 'react';
+import movies from 'prop-types';
+
+class MovieList extends Component {
+  render() {
+    const { title, subtitle, storyline, rating, imagePath } = this.props
+
+    return (
+      <div>
+        {`Título: ${title}`}
+        {`Subtítulo: ${subtitle}`}
+        {`Sinopse: ${storyline}`}
+        {`Avaliação: ${rating}`}
+        <img src={imagePath} alt=""></img>
+      </div>
+    )
+  }
+}
+// MovieList.prototype = {
+//   title: movies.string,
+//   subtitle: movies.string,
+//   storyline: movies.string,
+//   rating: movies.string,
+//   imagePath: movies.string,
+// }
+export default MovieList;
