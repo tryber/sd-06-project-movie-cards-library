54e16e566d4302848a81057e36b3c7ac12e8f4d2
HCaetano <hrcgeo@gmail.com>
2020-09-11 18:35:03 -0300

    Add 'Rating' component

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Rating from './Rating';
 import PropTypes from 'prop-types';
 
 class MovieCard extends Component {
   render() {
     const { movie } = this.props;
-    const { imagePath, title, subtitle, storyline } = movie;
+    const { imagePath, title, subtitle, storyline, rating } = movie;
 
     return (
       <div>
@@ -12,6 +13,7 @@ class MovieCard extends Component {
         <h5>{subtitle}</h5>
         <p>{storyline}</p>
         <img src={imagePath} alt="" />
+        <Rating rating={rating} />
       </div>
     );
   }
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,15 @@
-// implement Rating component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Rating extends Component {
+  render() {
+    const { rating } = this.props;
+
+    return <p className="rating">{rating}</p>;
+  }
+}
+
+Rating.defaultProps = { rating: 0 };
+Rating.propTypes = { rating: PropTypes.string };
+
+export default Rating;
