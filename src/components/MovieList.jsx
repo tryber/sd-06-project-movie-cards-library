add45b66c21848f799e1745e7ad0f2dbad0e31fd
Luciano Scalfone <luciano.scalfone@gmail.com>
2020-09-11 16:41:34 -0300

    projeto finalizado

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,23 @@
-// implement MovieList component here
+import React from 'react';
+import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
+
+
+class MovieList extends React.Component {
+  render() {
+    const movies = this.props.movies;
+    return(
+      <section className="movie-list">
+        {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)};
+      </section>
+    );
+  }
+}
+
+MovieList.propTypes = {
+  movies: PropTypes.array,
+  key: PropTypes.string,
+  movie: PropTypes.object,
+}
+
+export default MovieList;
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,17 @@
-// implement Rating component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Rating extends React.Component {
+  render() {
+    const rating = this.props.rating;
+    return(
+      <div className="rating">{rating}</div>
+    );
+  }
+}
+
+Rating.propType = {
+  rating: PropTypes.number,
+}
+
+export default Rating;
