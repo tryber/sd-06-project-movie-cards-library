ce18f63420eff8f26ff430d0b4413073402db853
Daniel Moura <darthur.moura@gmail.com>
2020-09-11 18:08:39 -0300

    Created components MovieList and MovieCard

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,23 @@
-// implement MovieCard component here
+import React, { Component } from 'react';
+import Rating from './Rating';
+import '../App.css';
+
+class MovieCard extends Component {
+  render() {
+    const { movie } = this.props;
+
+    return (
+      <div className="movie-card" key={movie.title}>
+        <img alt="Movie poster" className="movie-card-image" src={movie.imagePath} />
+        <div className="movie-card-body">
+          <h4 className="movie-card-title">{movie.title}</h4>
+          <h5 className="movie-card-subtitle">{movie.subtitle}</h5>
+          <p className="movie-card-storyline">{movie.storyline}</p>
+        </div>
+        <Rating rating={movie.rating} />
+      </div>
+    );
+  }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,20 @@
-// implement MovieList component here
+import React, { Component } from 'react';
+import MovieCard from './MovieCard.jsx';
+import movies from '../data.js';
+import '../App.css';
+
+class MovieList extends Component {
+  render() {
+    return (
+      <div className="movie-list">
+        {movies.map((movie) => {
+          return <MovieCard
+          movie={movie}
+          />
+        })};
+      </div>
+    );
+  }
+}
+
+export default MovieList;
