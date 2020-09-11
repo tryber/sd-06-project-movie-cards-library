2ff7f16041565427ea6d94bcd644b3c0fc22b490
Anderson R <misterregisslv@gmail.com>
2020-09-11 17:46:33 -0300

    codeClimate atualizalçao

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,26 +1,27 @@
 import React, { Component } from 'react';
 // import Rating from './Rating';
-import propTypes from 'prop-types'
+import propTypes from 'prop-types';
 
 class MovieCard extends Component {
-	render() {
-		const {image, title, subtitle, storyline} = this.props;
-		return(
-		<div className='movie-card'>
-			<img src={image} alt={`imagem do filme ${title}`} height='160px'/>
-			<h4>{title}</h4>
-			<h5>{subtitle}</h5>
-			<p>{storyline}</p>
-			{/* <Rating /> */}
-		</div>
-		)}
-}
+  render() {
+    const { image, title, subtitle, storyline } = this.props;
+    return (
+      <div className="movie-card">
+			  <img src={image} alt={`imagem do filme ${title}`} height="160px" />
+			  <h4>{title}</h4>
+			  <h5>{subtitle}</h5>
+			  <p>{storyline}</p>
+			  {/* <Rating /> */}
+		  </div>
+		);
+	};
+};
 
 MovieCard.propTypes = {
-	image: propTypes.string,
-	title: propTypes.string,
-	subtitle: propTypes.string,
-	storyline: propTypes.string,
+  image: propTypes.string,
+  title: propTypes.string,
+  subtitle: propTypes.string,
+  storyline: propTypes.string,
 }
 
 export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -3,18 +3,18 @@ import movies from '../data';
 import MovieCard from './MovieCard';
 
 class MovieList extends Component {
-	render() {
-		return (
-			movies.map(movieTitle => <MovieCard
-				key={movieTitle.title}
-				image={movieTitle.imagePath}
-				title={movieTitle.title}
-				subtitle={movieTitle.subtitle}
-				storyline={movieTitle.storyline}
+  render() {
+	  return (
+		  movies.map(movieTitle => <MovieCard
+			  key={movieTitle.title}
+			  image={movieTitle.imagePath}
+			  title={movieTitle.title}
+			  subtitle={movieTitle.subtitle}
+			  storyline={movieTitle.storyline}
 				/>
-			)
-		);
-	};
-};
+		  )
+	  );
+  }
+}
 
 export default MovieList;
