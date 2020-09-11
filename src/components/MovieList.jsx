0e71b96aae8f50a90f861ed5d7495624cce574b1
Matheus Coutinho <matheuscavalcante88@hotmail.com>
2020-09-11 15:15:37 -0300

    MovieList component

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,11 +1,15 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import MovieCard from './MovieCard';
 
 export default class MovieList extends Component {
   render() {
+    const { movies } = this.props;
     return (
-      <div>
-        
-      </div>
-    )
+      <>
+        {movies.map((movie) => (
+          <MovieCard movie={movie} key={movie.title} />
+        ))}
+      </>
+    );
   }
 }
