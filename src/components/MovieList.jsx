7fbe5ba13585863e780f33bc6dd5fc761d5f22d7
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2020-09-11 16:52:18 -0300

    Add render components MovieCard in MovieList

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import MovieCard from './MovieCard';
 
 class MovieList extends Component {
   render() {
+    const movies = this.props.movies;
     return (
-      <div> {this.props.movies} </div>
+      movies.map((movie) => (
+        <MovieCard movies={movie} />
+      ))
     );
   }
 }
