4e4c6da2da58930de0348ba65007b301a9dce6ae
Matheus Coutinho <matheuscavalcante88@hotmail.com>
2020-09-11 15:26:56 -0300

    Rating component

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,11 +1,12 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export default class Rating extends Component {
   render() {
+    const { rating } = this.props;
     return (
       <div>
-        
+        <span className="rating">{rating}</span>
       </div>
-    )
+    );
   }
 }
