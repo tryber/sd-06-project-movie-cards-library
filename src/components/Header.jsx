a0dd3b283daefc78786c3129d490559ad6395709
Pablo Santos <pablo.trybe@gmail.com>
2020-09-11 15:07:29 -0300

    Text rendered inside a h1 tag

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class Header extends Component {
   render() {
-    return <h1>Movie Cards Library</h1>;
+    return <header><h1>Movie Cards Library</h1></header>;
   }
 }
 
