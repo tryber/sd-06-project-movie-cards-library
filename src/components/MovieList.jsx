d37561b288ddce125ea49c94e33185a5380bbedc
Lais Gurgel <laisgurgelf@gmail.com>
2020-09-11 14:11:33 -0300

    Componente MovieList

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,13 @@
-// implement MovieList component here
+import React from 'react';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      <section>
+      <h1>MovieList</h1>
+      </section>
+    )
+  }
+}
+
+export default MovieList;
