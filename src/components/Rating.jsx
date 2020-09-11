5581349ae30f6d9c30f7761f1cdf69d256ba0ee0
Julia Porto <juportob@gmail.com>
2020-09-11 18:18:30 -0300

    Adding empty line

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+
 class Rating extends Component {
   render() {
+    const { rating } = this.props
     return (
       <div className="rating">(rating)</div>
     );
