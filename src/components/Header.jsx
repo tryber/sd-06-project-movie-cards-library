c250de8b26f57b99c582d3482dd65e9786374a84
RenatoCostaDev <renatotrybe@gmail.com>
2020-09-11 16:31:19 -0300

    02- requisitos 1 e 2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,13 @@
 import React from 'react';
 
-export default () => {
-  return (
-    <header>
-      <h1>Movie Cards Library</h1>
-    </header>
-  )
+class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <h1>Movie Cards Library</h1>
+      </header>
+    );
+  }
 }
 
-// class Header extends React.Component {
-//   render() {
-//     return (
-//       <header>
-//         <h1>Movie Cards Library</h1>
-//       </header>
-//     );
-//   }
-// }
-
-// export default Header;
+export default Header;
