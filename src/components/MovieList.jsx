9668b440ef6381fa22eed0c6333f2b37ce0198d9
Ana Flávia <anaflvia@gmail.com>
2020-09-11 17:58:03 -0300

    Corrigindo cc 2

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -14,7 +14,8 @@ class MovieList extends Component {
             title={movie.title}
             subtitle={movie.subtitle}
             synopsis={movie.storyline}
-            rating={movie.rating} />)
+            rating={movie.rating} />
+        );
           )}
       </div>
     );
