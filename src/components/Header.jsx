0756fd6b7bb74bfb3a7e13c7bce5fee87e418e0a
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2020-09-11 15:46:06 -0300

    Adding first and second requirement (CC check)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,14 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-
 
 class Header extends React.Component {
   render() {
-
     const headerText = 'Movie Cards Library';
-
     return (
       <header>
         <h1>{headerText}</h1>
       </header>
-    )
+    );
   }
-}
-
-Header.propTypes = {
-  headerText: PropTypes.string,
 }
 
 export default Header;
