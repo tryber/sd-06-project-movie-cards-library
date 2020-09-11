883f5f2faeab64f93cfe10faa987a6d453dd217e
Arthur de Sa <arthurdesa94@gmail.com>
2020-09-11 16:47:29 -0300

    rendering Header and App OK

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,2 +1,15 @@
-// implement Header component here
-// Iniciando o projeto 🚀
+import React from 'react'
+import Proptypes from 'prop-types';
+class Header extends React.Component {
+    render () {
+        return
+        (<div className="movie-card-header">
+            <h1 className="page-title">
+                Movie Cards Library
+            </h1>
+        </div>)
+    }
+}
+
+export default Header;
+
