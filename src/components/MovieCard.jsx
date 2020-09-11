1654833e784d2ff32a79d29711c0c7e465f25dea
HCaetano <hrcgeo@gmail.com>
2020-09-11 18:16:52 -0300

    Add 'Render movie storyline' requirement

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,12 +4,13 @@ import PropTypes from 'prop-types';
 class MovieCard extends Component {
   render() {
     const { movie } = this.props;
-    const { imagePath, title, subtitle } = movie;
+    const { imagePath, title, subtitle, storyline } = movie;
 
     return (
       <div>
         <h4>{title}</h4>
         <h5>{subtitle}</h5>
+        <p>{storyline}</p>
         <img src={imagePath} alt="" />
       </div>
     );
