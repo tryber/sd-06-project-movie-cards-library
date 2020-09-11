bd24b74131b451b1d01fdc00a4f924c1d2273bac
Bruna Franciele <brunafranciele1902@gmail.com>
2020-09-11 18:15:48 -0300

    envio do requisito de render

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -3,8 +3,9 @@ import PropTypes from 'prop-types';
 
 class Rating extends Component {
   render() {
+    const { rating } = this.props;
     return (
-      <div className="rating">(rating)</div>
+      <div className="rating">{rating}</div>
     );
   }
 }
