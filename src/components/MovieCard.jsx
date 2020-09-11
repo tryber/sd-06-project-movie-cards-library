481c21ad29f11b65bcf2cc755697e285c555a9cc
rafMewtwo <ponciano.raf@gmail.com>
2020-09-11 19:18:38 -0300

    fala cmg

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -5,16 +5,16 @@ import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
-      const {title, subtitle, storyline, rating, imagePath} = this.props.movie;
-      return (
-          <div>
-              <h4>{title}</h4>
-              <h5>{subtitle}</h5>
-              <p>{storyline}</p>
-              <Rating rating={rating} />
-              <img src={imagePath}></img>
-          </div>
-        )
+    const {title, subtitle, storyline, rating, imagePath} = this.props.movie;
+    return (
+        <div>
+            <h4>{title}</h4>
+            <h5>{subtitle}</h5>
+            <p>{storyline}</p>
+            <Rating rating={rating} />
+            <img src={imagePath}></img>
+        </div>
+    )
     }
 }
 MovieCard.propTypes = {
