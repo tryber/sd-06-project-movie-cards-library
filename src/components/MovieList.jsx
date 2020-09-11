c5ee3fdfe68b40e82aa3ba476bca52e924976fda
Bruna Franciele <brunafranciele1902@gmail.com>
2020-09-11 15:56:07 -0300

    consertando cc sobre movieList e movieCard

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,23 +1,21 @@
 import React, { Component } from 'react';
 import movies from './data';
 import MovieCard from './MovieCard';
-
 class MovieList extends Component {
   render() {
     return (
-      <div className="pokedex">
-        {movies.map((movie) => {
-          return (<MovieCard
+      <div className='movielist'>
+        {movies.map((movie) => (
+          <MovieCard
             key={movie.title}
             title={movie.title}
             subtitle={movie.subtitle}
             storyline={movie.storyline}
             imagePath={movie.imagePath}
-          />);
-        })}
+          />
+        ))}
       </div>
     );
   }
 }
-
 export default MovieList;
