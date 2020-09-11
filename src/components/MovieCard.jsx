3ce0f467905851da20f9eb8554520638a6a5bae8
Bruna Franciele <brunafranciele1902@gmail.com>
2020-09-11 15:21:20 -0300

    requisitos sobre movieList e movieCard

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,29 @@
-// implement MovieCard component heree
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+class MovieCard extends Component {
+  render() {
+    const { title, subtitle, storyline, imagePath } = this.props
+
+    return (
+      <div className="pokemon">
+        <div>
+          <img src={imagePath} alt={title} />
+          <h4>{title}</h4>
+          <h5>{subtitle}</h5>
+          <p>{storyline}</p>
+        </div>
+        <br />
+      </div>
+    );
+  }
+}
+
+MovieCard.propTypes = {
+  title: PropTypes.string,
+  subtitle: PropTypes.string,
+  storyline: PropTypes.string,
+  imagePath: PropTypes.string
+}
+
+export default MovieCard
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,23 @@
-// implement MovieList component here
+import React, { Component } from 'react'
+import movies from './data'
+import MovieCard from './MovieCard'
+
+class MovieList extends Component {
+  render() {
+    return (
+      <div className="pokedex">
+        {movies.map((movie) => {
+          return <MovieCard
+            key={movie.title}
+            title={movie.title}
+            subtitle={movie.subtitle}
+            storyline={movie.storyline}
+            imagePath={movie.imagePath}
+          />
+        })}
+      </div>
+    );
+  }
+}
+
+export default MovieList
