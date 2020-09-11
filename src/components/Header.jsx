ff155157fd97ddc2ef1add94ab14e2a1a0204bc1
William <williamjog@hotmail.com>
2020-09-11 15:10:13 -0300

    1st requirement | creating Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,12 @@
-//Insira o seu código aqui champz!
+import React from 'react';
+
+
+class Header extends React.Component {
+  render() {
+    return (
+      <span>Oi, eu não sei fazer nada!</span>
+    );
+  }
+}
+
+export default Header;
