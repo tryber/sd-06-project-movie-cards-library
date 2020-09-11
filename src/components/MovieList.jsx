33bab1dba6299c9140fb0b16613a5c961251adc3
Isabela Rosa <isabelarosa954@gmail.com>
2020-09-11 15:43:51 -0300

    criando e renderizando movie list

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,17 @@
-// implement MovieList component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <h1>Movie List</h1>
+    );
+  }
+}
+
+MovieList.propTypes = {
+  movies: PropTypes.array,
+};
+
+export default MovieList;
