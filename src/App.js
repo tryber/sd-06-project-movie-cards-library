49690a1f1698120bdf72b2cffa537dee9dc087e5
Anderson R <misterregisslv@gmail.com>
2020-09-11 18:06:14 -0300

    codeclimate 5

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import './components/Header'
+import './components/Header';
 import Header from './components/Header';
 import MovieList from './components/MovieList';
 
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -5,13 +5,12 @@ import MovieCard from './MovieCard';
 class MovieList extends Component {
   render() {
     return (
-		  movies.map((movieTitle) => <MovieCard
+      movies.map((movieTitle) => <MovieCard
         key={movieTitle.title}
         image={movieTitle.imagePath}
         title={movieTitle.title}
         subtitle={movieTitle.subtitle}
-        storyline={movieTitle.storyline}
-        />
+        storyline={movieTitle.storyline} />
 			)
     );
   }
