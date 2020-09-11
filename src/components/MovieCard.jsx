fe4b9181cc8559fc258d2f035445c5d9442d5061
rafMewtwo <ponciano.raf@gmail.com>
2020-09-11 18:28:11 -0300

    codeclimate

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,18 +1,18 @@
 // implement MovieCard component here
 import React from 'react';
-import Rating from './Rating';
 import PropTypes from 'prop-types';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
-    render() {
-        return (
-            <div>
-                <h4>{this.props.movie.title}</h4>
-                <h5>{this.props.movie.subtitle}</h5>
-                <p>{this.props.movie.storyline}</p>
-                <Rating rating={this.props.movie.rating} />
-                <img src={this.props.movie.imagePath}></img>
-            </div>
+  render() {
+      return (
+          <div>
+              <h4>{this.props.movie.title}</h4>
+              <h5>{this.props.movie.subtitle}</h5>
+              <p>{this.props.movie.storyline}</p>
+              <Rating rating={this.props.movie.rating} />
+              <img src={this.props.movie.imagePath}></img>
+          </div>
         )
     }
 }
