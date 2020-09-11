99242a9937bbb1b25c16ac1caff395d1ebfd2781
Leandro <leandro.feitoza8018@gmail.com>
2020-09-11 17:52:40 -0300

    Renderizado o MovieCard na tela, e corrigido erro de PropTypes, na declaraçao

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,27 @@
-// implement MovieCard component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class MovieCard extends Component {
+  render() {
+    const { title, subtitle, storyline, imagePath, rating } = this.props;
+    return (
+      <div>
+        <h4> {title} </h4>
+        <h5> { subtitle }</h5>
+        <p>{storyline}</p>
+        <img src={imagePath} alt="Poster do Filme" />
+        {rating}
+      </div>
+    );
+  }
+}
+
+MovieCard.propTypes = {
+  title: PropTypes.string.isRequired,
+  subtitle: PropTypes.string.isRequired,
+  storyline: PropTypes.string.isRequired,
+  imagePath: PropTypes.node.isRequired,
+  rating: PropTypes.number.isRequired,
+};
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,25 @@
-// implement MovieList component here
+import React, { Component } from 'react';
+import movies from '../data.js';
+import MovieCard from './MovieCard.jsx';
+
+class MovieList extends Component {
+  render() {
+    return (
+      movies.map((movie) =>
+         (
+           <div>
+             <MovieCard
+              //  key={index}
+               title={movie.title}
+               subtitle={movie.subtitle}
+               storyline={movie.storyline}
+               imagePath={movie.imagePath}
+               rating={movie.rating}
+             />
+           </div>
+      ))
+    );
+  }
+}
+
+export default MovieList;
