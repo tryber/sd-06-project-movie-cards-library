d2095416e3e86462a1773999a171cbe0f0b94b92
Julia Porto <juportob@gmail.com>
2020-09-11 15:23:20 -0300

    Adding requirements about movieList

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,28 @@
-// implement MovieCard component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class MovieCard extends Component {
+  render() {
+    const { title, subtitle, storyline, imagePath } = this.props
+    return (
+      <div className="moviecard">
+        <div>
+          <img src={imagePath} alt={`Movie: ${title}`} />
+        </div>
+        <br />
+          <h4>{title}</h4>
+          <h5>{subtitle}</h5>
+          <p>{storyline}</p>
+      </div>
+    );
+  }
+}
+
+MovieCard.propTypes = {
+  title: PropTypes.string.isRequired,
+  subtitle: PropTypes.string.isRequired,
+  storyline: PropTypes.string.isRequired,
+  imagePath: PropTypes.string.isRequired,
+}
+
+export default MovieCard;
