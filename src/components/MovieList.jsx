b634cb824635264768f592affda5d6a8c1394505
Anderson R <misterregisslv@gmail.com>
2020-09-11 18:01:58 -0300

    codeclimate 4

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -4,16 +4,16 @@ import MovieCard from './MovieCard';
 
 class MovieList extends Component {
   render() {
-	  return (
-		  movies.map(movieTitle => <MovieCard
-			  key={movieTitle.title}
-			  image={movieTitle.imagePath}
-			  title={movieTitle.title}
-			  subtitle={movieTitle.subtitle}
-			  storyline={movieTitle.storyline}
-				/>
-		  )
-	  );
+    return (
+		  movies.map((movieTitle) => <MovieCard
+        key={movieTitle.title}
+        image={movieTitle.imagePath}
+        title={movieTitle.title}
+        subtitle={movieTitle.subtitle}
+        storyline={movieTitle.storyline}
+        />
+			)
+    );
   }
 }
 
