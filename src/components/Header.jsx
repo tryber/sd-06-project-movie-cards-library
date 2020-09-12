1895e722b618dc98a151d4016f23b426909337e6
STEFANO-BRANZ <stefano.branz@gmail.com>
2020-09-12 18:49:03 -0300

    First and second requirements push 04

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-class Header extends React.Component {
+class Header extends Component {
   render() {
     // Retorno meu header da forma como quero e exporto para ser importado pelo 'app.js'
     return (
