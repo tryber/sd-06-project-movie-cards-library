f7728e676c2f2f300a80e4d3cdb97f0a0bcb5325
Alessandra <itsalematos@hotmail.com>
2020-09-12 08:12:14 -0400

    Creating movieList component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 
+
 class Header extends React.Component {
   render() {
     return (
-      <h1>Movie Cards Library</h1>
+      <h1>"Movie Cards Library"</h1>
     );
   }
 }
