beb3b1e46d86cbc5da506ad6c38cce34c4f11cc7
Pedro Duarte <ikbenpedro@gmail.com>
2020-09-12 10:54:49 -0300

    cc 1.0

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,22 @@
 // implement MovieCard component here
+import React, { Component } from 'react'
+// import PropTypes from 'prop-types'
+import Rating from './Rating'
+
+class MovieCard extends Component {
+  render() {
+    const { title, subtitle, storyline, Rating, imagePath } = this.props.movie;
+    return (
+					<div className='movie-card'>
+							<img className='movie-card-image' src={imagePath} alt="imagem-filme" />
+							<h4 className="movie-card-title">{title}</h4>
+							<h5 className="movie-card-subtitle">{subtitle}</h5>
+							<p className="movie-card-storyline">{storyline}</p>
+							<a className="rating">rating={Rating}</a>
+					</div>
+    )
+  }
+}
+
+
+export default MovieCard
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,15 @@
 // implement Rating component here
+import React from 'react';
+
+class Rating extends React.Component {
+	render() {
+		return (
+		<div>
+		<a>rating={Rating}</a>
+		</div>
+	
+		);
+	}
+}
+
+export default Rating;
