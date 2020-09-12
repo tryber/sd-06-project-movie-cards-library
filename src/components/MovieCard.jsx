24f5de3414724b3f9ad224713a42f398bd4140ae
LeonardoCavachini <cavachini240@gmail.com>
2020-09-12 10:38:16 -0300

    CC update

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -5,20 +5,19 @@ import Rating from './Rating';
 
 class MovieCard extends Component {
   render() {
-    const { title, subtitle, storyline, imagePath, rating } = this.props.movie;
+    const { title, subtitle, storyline, imagePath, rating }=this.props.movie;
     return (
       <div className="movie-card-body">
-        <img className="movie-card-image" src = {imagePath} alt="finlaFantasy" />
+        <img className="movie-card-image" src={imagePath} alt="finlaFantasy" />
         <h4 className="movie-card-title">{title}</h4>
         <h5 className="movie-card-subtitle">{subtitle}</h5>
         <p className="movie-card-storyline">{storyline}</p>
-        <Rating className="movie-card-rating" rating = {rating} />
+        <Rating className="movie-card-rating" rating={rating} />
       </div>
     );
   }
 }
-MovieCard.propTypes = 
-{
+MovieCard.propTypes = {
   title: PropTypes.string.isRequired,
   subtitle: PropTypes.string.isRequired,
   storyline: PropTypes.string.isRequired,
