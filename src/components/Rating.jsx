6600b8ce4176805408977230d8120c8d36bf9b2b
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2020-09-11 22:33:24 -0300

    Add rating component

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,11 @@
-// implement Rating component here
+import React, { Component } from 'react';
+
+class Rating extends Component {
+  render() {
+    return (
+      <div></div>
+    );
+  }
+}
+
+export default Rating;
