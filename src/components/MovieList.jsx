f6d72c0d7d02a41ebff957d9a4a46bbea26ecafb
FlavioSugano <flavio.sugano@gmail.com>
2020-09-12 12:06:20 -0300

    Correções do Code Climate

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -4,8 +4,7 @@ import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
-    return this.props.moviesFromData
-      .map((movie) => <MovieCard key={movie.title} movies={movie} />);
+    return this.props.moviesFromData.map((movie) => <MovieCard key={movie.title} movies={movie} />);
   }
 }
 
