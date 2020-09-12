9f91095b324bd3271ed3534436c6c7da7cd39c06
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2020-09-12 00:27:35 -0300

    Add proptype of rating

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Rating extends Component {
   render() {
     const { rating } = this.props;
     return (
-      <div className="rating">{rating}</div>
+      <div className="movie-card-rating">
+        <span className="rating">{rating}</span>
+      </div>
     );
   }
 }
+
+Rating.propTypes = { rating: PropTypes.number.isRequired };
 
 export default Rating;
