8b568ce8654407e12c29f81ccac4c399e2bae800
FlavioSugano <flavio.sugano@gmail.com>
2020-09-11 23:13:10 -0300

    Header.jsx: pequenas alterações no texto

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,8 @@
-// implement Header component here
 import React from 'react';
-
 
 class Header extends React.Component {
   render() {
-    return <header className="movie-card-header"><h1>Movie Cards Library</h1></header>;
+    return <header className="movie-card-header"><h1 className="page-title">Movie Cards Library</h1></header>;
   }
 }
 
