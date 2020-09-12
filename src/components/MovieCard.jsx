4f74d938c002aa207fd60220adb9605ba1513654
Igor Leão <igorleao.eng@gmail.com>
2020-09-12 11:01:42 -0300

    cc analize

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -7,11 +7,11 @@ class MovieCard extends Component {
     const { title, subtitle, storyline, rating, imagePath } = this.props.movie;
 
     return (
-      <div className='movie-card'>
-        <img className='movie-card-image' alt='image-descripton' src={imagePath} />
-        <h4 className='movie-card-title'>{title}</h4>
-        <h5 className='movie-card-subtitle'>{subtitle}</h5>
-        <p className='movie-card-storyline'>{storyline}</p>
+      <div className="movie-card">
+        <img className="movie-card-image" alt="image-descripton" src={imagePath} />
+        <h4 className="movie-card-title">{title}</h4>
+        <h5 className="movie-card-subtitle">{subtitle}</h5>
+        <p className="movie-card-storyline">{storyline}</p>
         <Rating rating={rating} />
       </div>
     );
