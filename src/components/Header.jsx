04337515e407cfb2f80d19460cbad146e3383a27
Alessandra <itsalematos@hotmail.com>
2020-09-12 09:40:24 -0400

    Rendering title on Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,10 @@ import React from 'react';
 class Header extends React.Component {
   render() {
     return (
-      <h1>Movie Cards Library</h1>
+      <header>
+        <h1>Movie Cards Library</h1>
+      </header>
+      
     );
   }
 }
