61103bc895da0bcaa2b5123a29d170c37077d5bc
Daniele Perse <danieleperse3@gmail.com>
2020-09-12 19:00:03 -0300

    add Proptypeno arquivo Rating

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Rating from './Rating';
 
 class MovieCard extends React.Component {
@@ -19,6 +20,10 @@ class MovieCard extends React.Component {
       </div>
     );
   }
+}
+
+MovieCard.propTypes = {
+  movie: PropTypes.string,
 }
 
 export default MovieCard;
