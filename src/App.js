d79c02bd1929411c7e6a1028a8b43f893655c93b
William <williamjog@hotmail.com>
2020-09-11 21:06:59 -0300

    3rd requirement | Adding MovieList

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 import Header from '../src/components/Header';
+import MovieList from '../src/components/MovieList';
+import movies from './data';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <MovieList movies={movies} />
     </div>
   );
 }
