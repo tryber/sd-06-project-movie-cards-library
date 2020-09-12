88ab9b9ad846edf2d332861cae09647b941cceb5
FlavioSugano <flavio.sugano@gmail.com>
2020-09-12 02:17:09 -0300

    MovieList.jsx: adição de propType e defaultProps

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import PropType from 'prop-types';
 import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
-    return this.props.moviesFromData.map(
-      (movie) => <MovieCard key="movie.title" movies={movie} />
-    );
+    return this.props.moviesFromData
+      .map((movie) => <MovieCard key={movie.title} movies={movie} />);
   }
 }
+
+MovieList.propType = { moviesFromData: PropType.arrayOf(PropType.object) };
+MovieList.defaultProps = { moviesFromData: {} };
 
 export default MovieList;
