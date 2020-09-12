bf3b05b5227b83f3e477ae67c3a7b04ac3b13bbb
FlavioSugano <flavio.sugano@gmail.com>
2020-09-11 23:35:34 -0300

    Requisitos 13 a 16

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -3,7 +3,7 @@ import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
-    return this.props.moviesFromData.map(movie => <MovieCard key="movie.title" movies={movie} />);
+    return this.props.moviesFromData.map((movie='') => <MovieCard key="movie.title" movies={movie} />);
   }
 }
 
