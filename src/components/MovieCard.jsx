6262c64b85ba3c25b2afaa31d42eb3e61b4d18b5
Cássio Rubens <cassiorubensadv@gmail.com>
2020-09-12 17:16:27 -0300

    Key MovieCard

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,24 +1,25 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import movieType from '../Types';
 import Rating from './Rating';
 
 class MovieCard extends Component {
   render() {
-    // const { movie } = this.props;
-    const { title, subtitle, storyline, rating, imagePath } = this.props.movie;
+    const { movie } = this.props;
+    // const { title, subtitle, storyline, rating, imagePath } = this.props.movie;
 
     return (
       <section className="movie-card">
-        <img src={imagePath} alt={title} />
-        <h4>{title}</h4>
-        <h5>{subtitle}</h5>
-        <p>{storyline}</p>
-        <Rating rating={rating} />
+        <img src={movie.imagePath} alt={movie.title} />
+        <h4>{movie.title}</h4>
+        <h5>{movie.subtitle}</h5>
+        <p>{movie.storyline}</p>
+        <Rating rating={movie.rating} />
       </section>
     );
   }
 }
 
-MovieCard.propTypes = movieType;
+MovieCard.propTypes = PropTypes.shape(movieType).isRequired;
 
 export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -12,11 +12,7 @@ class MovieList extends Component {
         {movies.map((movie) => (
           <MovieCard
             key={movie.title}
-            title={movie.title}
-            subtitle={movie.subtitle}
-            storyline={movie.storyline}
-            rating={movie.rating}
-            imagePath={movie.imagePath}
+            movie={movie}
           />
         ))}
       </section>
