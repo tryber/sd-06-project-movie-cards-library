5073ad73d2a162426189fb010b157c50db3d5d2b
FlavioSugano <flavio.sugano@gmail.com>
2020-09-11 23:14:24 -0300

    MovieList.jsx: criação da estrutura para gerar o MovieCard de cada filme

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,10 @@
-// implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    return this.props.moviesFromData.map(movie => <MovieCard key="movie.title" movies={movie} />);
+  }
+}
+
+export default MovieList;
