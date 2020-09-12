84c1f061a1b483ad3083f43191ffb9a37b8a576d
Artur Forna <fornagieri.artur@gmail.com>
2020-09-12 13:30:00 -0300

    Iniciando o Projeto :godmode:

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,14 @@
-// implement Header component here
+import React from 'react';
+
+import MovieCard from './MovieCard'; // teste
+class Header extends React.Component {
+  render() {
+    return (
+    <header>
+      <h1>Movie Cards Library</h1>
+      <MovieCard />
+    </header>)
+  }
+}
+
+export default Header;
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,11 @@
-// implement MovieCard component here
+import React from 'react';
+
+import movies from '../data';
+
+class MovieCard extends React.Component {
+  render() {
+  return <h1>{movies[0].title}</h1> // pq um object?
+  }
+}
+
+export default MovieCard;
