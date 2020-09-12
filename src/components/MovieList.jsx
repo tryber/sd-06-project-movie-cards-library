967aa96005783fd77487fcb2f21843aaa4faf6f4
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2020-09-12 00:29:51 -0300

    Add style

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -6,9 +6,11 @@ class MovieList extends Component {
   render() {
     const { movies } = this.props;
     return (
-      movies.map((movie) => (
-        <MovieCard key={movie.title} movie={movie} />
-      ))
+      <div className="movie-list">
+        {movies.map((movie) => (
+          <MovieCard key={movie.title} movie={movie} />
+        ))}
+      </div>
     );
   }
 }
