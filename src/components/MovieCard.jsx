aeeb43b137a558ad9f775f638f2192ee60ca3b8f
DekoPossas <andrepossaster@gmail.com>
2020-09-12 14:51:19 -0300

    commit de test

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,32 @@
 // implement MovieCard component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+// import movies from '../data';
+
+class MovieCard extends Component {
+  render() {
+    const { title, subtitle, storyline, rating, imagePath } = this.props;
+    return (
+      <span>
+        Titulo: {title}
+        <br />
+        SubTitulo: {subtitle}
+        <br />
+        Prologo: {storyline}
+        <br />
+        Nota: {rating}
+        <br />
+        <img src={imagePath}></img>
+        <br />
+        <br />
+      </span>
+    );
+  }
+}
+
+MovieCard.propTypes = {
+  title: PropTypes.string,
+  subtitle: PropTypes.string
+};
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,27 @@
 // implement MovieList component here
+import React, { Component } from 'react';
+import movies from '../data';
+import MovieCard from './MovieCard';
+
+class MovieList extends Component {
+  render() {
+    return (
+      <div>
+        {movies.map((movie, index) => {
+          return (
+            <MovieCard
+              key={index}
+              title={movie.title}
+              subtitle={movie.subtitle}
+              storyline={movie.storyline}
+              rating={movie.rating}
+              imagePath={movie.imagePath}
+            />
+          );
+        })}
+      </div>
+    );
+  }
+}
+
+export default MovieList;
