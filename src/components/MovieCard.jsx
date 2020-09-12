a9f891c36988e563fbba01a647241d5d1abc8d26
rodrigoandrademelo <rodrigoandrademelo@gmail.com>
2020-09-11 21:31:09 -0300

    in progress

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,15 @@
+import { render } from 'enzyme';
 // implement MovieCard component here
+import React from 'react';
+class MovieCard React.Component {
+  render () {
+    const films = this.props.movies;
+    return (
+      <section>
+
+      </section>
+    );
+  }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,17 @@
 // implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render () {
+    const films = this.props.movies;
+    return (
+      <div>
+        {(films.map((movie) => {
+          <MovieCard movie={movie} />
+        }))}
+      </div>
+    );
+  }
+}
+export default MovieList;
