d950c2b2cc9babc2b182b4e84882ee96e5239a72
Alessandra <itsalematos@hotmail.com>
2020-09-12 09:33:13 -0400

    Code Climate

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 class Header extends React.Component {
   render() {
     return (
-      <h1>"Movie Cards Library"</h1>
+      <h1>Movie Cards Library</h1>
     );
   }
 }
