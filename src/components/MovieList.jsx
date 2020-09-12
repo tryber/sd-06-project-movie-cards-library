893fda3f7930c0f5178b84502a66a3e931febe20
Alessandra <itsalematos@hotmail.com>
2020-09-12 14:30:54 -0400

    Adding rating component

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import MovieCard from './MovieCard';
-import movies from '../data';
+import PropTypes from 'prop-types'
 
 class MovieList extends React.Component {
   render() {
     return (
       <div>
-        {movies.map((movie) =>
+        {this.props.movies.map((movie) =>
           <MovieCard key={movie.title} movie={movie} />,
         )}
       </div>
     );
   }
 }
+
+MovieList.propTypes = { movies: PropTypes.array }
 
 export default MovieList;
