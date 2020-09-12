a55ab39149217a2b50c728e3251d52b4b53498b3
flaviolunaferreira <flaviolunaferreira@gmail.com>
2020-09-12 08:05:09 -0300

    ninguem se mexe... parece que agora vai...

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,15 +4,15 @@ import theCoyote from '../img/1.png';
 class Header extends React.Component {
   render() {
     return (
-      <div className="movie-card-header">
-        <h1 className="titulo">Movie Cards Library</h1>
+      <header className="movie-card-header">
+        <h1 className="page-title">Movie Cards Library</h1>
         <div className="cubo">
           <img src={theCoyote} alt="imagem" width="60" />
           <img src={theCoyote} alt="girando" width="60" />
           <img src={theCoyote} alt="com o logo" width="60" />
           <img src={theCoyote} alt="do coyote" width="60" />
         </div>
-      </div>
+      </header>
     );
   }
 }
