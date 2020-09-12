1c88f7512f0aa0ebe323de8212899280a5a4c418
FlavioSugano <flavio.sugano@gmail.com>
2020-09-11 23:41:01 -0300

    MovieCar.jsx e Rating.jsx: ajustes do Code Climate

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,16 +3,16 @@ import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
-    const { title='', subtitle='', storyline='', rating='0', imagePath='' } = this.props.movies;
+    const { title = '', subtitle = '', storyline = '', rating = '0', imagePath = '' } = this.props.movies;
     return (
       <div className="movie-card">
-        <img className="movie-card-image" src={imagePath} />
+        <img className="movie-card-image" src={imagePath} alt={title} />
         <div className="movie-card-body">
           <h4 className="movie-card-title">{title}</h4>
           <h5 className="movie-card-subtitle">{subtitle}</h5>
           <p className="movie-card-storyline">{storyline}</p>
         </div>
-        <Rating rating={rating}/>
+        <Rating rating={rating} />
 
       </div>
     );
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -4,10 +4,10 @@ import PropTypes from 'prop-types';
 class Rating extends React.Component {
   render() {
     return (
-      <div className="movie-card-rating"> 
+      <div className="movie-card-rating">
         <span className="rating">{this.props.rating}</span>
       </div>
-    )
+    );
   }
 }
 
