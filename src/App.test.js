082fea5db59e5805a025b61864da136bf9016fc2
Gabriel Luiz da Silva <gabrielsilva.adm@gmail.com>
2020-09-12 18:43:12 -0300

    add Header

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { mount, shallow } from 'enzyme';
-
 import App from './App';
 import Header from './components/Header';
 import MovieCard from './components/MovieCard';
