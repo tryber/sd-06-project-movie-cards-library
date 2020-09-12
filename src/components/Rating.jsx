0fd74edbdcea01f03b5584b5845365c2fe38a53e
FlavioSugano <flavio.sugano@gmail.com>
2020-09-11 23:16:42 -0300

    Rating.jsx: criação do arquivo que gera o rating no final do card

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,13 @@
-// implement Rating component here
+import React from 'react';
+
+class Rating extends React.Component {
+  render() {
+    return (
+      <div className="movie-card-rating"> 
+        <span className="rating">{this.props.rating}</span>
+      </div>
+    )
+  }
+}
+
+export default Rating;
