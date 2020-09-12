b0bc1d42acb143a4157b32067acff3ad64666ced
STEFANO-BRANZ <stefano.branz@gmail.com>
2020-09-12 18:27:41 -0300

    First and second requirements push 02

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,12 +3,12 @@ import React from 'react';
 class Header extends React.Component {
   render() {
     // Retorno meu header da forma como quero e exporto para ser importado pelo 'app.js'
-    return(
+    return (
       <header>
         <h1 className="title">Movie Cards Library</h1>
       </header>
-    );  
-}
+    ); 
+  }
 }
 
 export default Header;
