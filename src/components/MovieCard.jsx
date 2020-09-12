b788a2badce5ffa2a1ec314583231c06d622f6dc
Paulo <paulocheloni@gmail.com>
2020-09-12 12:33:13 -0300

    adding cs

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Rating from './Rating';
+import '../App.css';
 
 class MovieCard extends Component {
   render() {
     const { title, subtitle, imagePath, storyline, rating } = this.props.movie;
     return (
-      <ul>
-        <li><h4>{title}</h4></li>
-        <li><h5>{subtitle}</h5></li>
-        <li><img src={imagePath} alt={`${title} pic`} /></li>
-        <li><p>{storyline}</p></li>
+      <div className="movie-card">
+        <img className="movie-card-image" src={imagePath} alt={`${title} pic`} />
+        <div className="movie-card-body">
+          <h4 className="movie-card-title">{title}</h4>
+          <h5 className="movie-card-subtitle">{subtitle}</h5>
+          <p className="movie-card-storyline">{storyline}</p>
+        </div>
         <Rating rating={rating} />
-      </ul>
+      </div>
     );
   }
 }
@@ -23,7 +26,7 @@ MovieCard.propTypes = {
     subtitle: PropTypes.string,
     imagePath: PropTypes.string,
     storyline: PropTypes.string,
-    rating: PropTypes.elementType,
+    rating: PropTypes.number,
   }),
 };
 
