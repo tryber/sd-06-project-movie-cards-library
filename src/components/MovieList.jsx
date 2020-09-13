4da352a9b7221bb69b44ed93a7eae081ac699bb9
STEFANO-BRANZ <stefano.branz@gmail.com>
2020-09-13 15:25:01 -0300

    Third requirement push 02

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import movieCard from './MovieCard';
+
 
 class MovieList extends Component {
   render() {
-    return;
+    const { movies } = this.props;
+    return (
+      <div className="movie-list">
+       blablabla
+      </div>
+    );
   }
 }
 
