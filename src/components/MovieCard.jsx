4fc5b51808b9985955580b9686caaf6dfe63f204
Bruno Lemos <brunolemos.ssa@gmail.com>
2020-09-13 15:59:53 -0300

    Correçoes de estrutura e estilos nos componentes MovieCard e Rating

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -11,7 +11,9 @@ class MovieCard extends React.Component {
           <h4 className="movie-card-title">{title}</h4>
           <h5 className="movie-card-subtitle">{subtitle}</h5>
           <p className="movie-card-storyline">{storyline}</p>
-          <Rating rating={rating} />
+        </div>
+        <div className="movie-card-rating">
+        <Rating rating={rating} />
         </div>
       </div>
     );
