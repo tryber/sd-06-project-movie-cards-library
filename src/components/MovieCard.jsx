41d316c28c4fea726160c81eb93e80b227a72746
Bruno Lemos <brunolemos.ssa@gmail.com>
2020-09-13 16:21:05 -0300

    Correçao da propriedade alt da tag img

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -7,7 +7,7 @@ class MovieCard extends React.Component {
     const { title, subtitle, storyline, imagePath, rating } = this.props.movie;
     return (
       <div className="movie-card">
-        <img className="movie-card-image" src={imagePath} alt={`${title} Movie Image`} />
+        <img className="movie-card-image" src={imagePath} alt={`${title} Movie`} />
         <div className="movie-card-body">
           <h4 className="movie-card-title">{title}</h4>
           <h5 className="movie-card-subtitle">{subtitle}</h5>
