b17f34e1e58d643d093c36ec2863c62bc6798a80
William <williamjog@hotmail.com>
2020-09-12 23:56:09 -0300

    Fixing CodeClimate Issues | PropsValidation

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,20 +1,9 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
     return ('oi');
   }
 }
-
-MovieCard.propTypes = {
-  movies: PropTypes.shape({
-    title: PropTypes.string,
-    subtitle: PropTypes.string,
-    storyline: PropTypes.string,
-    rating: PropTypes.number,
-    imagePath: PropTypes.string,
-  }).isRequired,
-};
 
 export default MovieCard;
