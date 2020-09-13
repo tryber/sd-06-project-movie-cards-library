47c8bdf1d39da62168c820a900ae656fc3632b47
CleberFontinele <cleberfontinele2387@gmail.com>
2020-09-13 15:49:22 -0300

    alguns requisitos renders

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,15 @@
 // implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+    render() {
+        return (<span>AAAAAAAAAAAAAAAAA</span>)
+    }
+}
+
+export default MovieCard;
+
+// Crie um componente chamado MovieList
+// Este componente representará toda a área com os cartões de filmes. 
+// MovieList deve receber uma prop movies, que é um array de objetos com 
+//informações de um filme.
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,19 @@
-// implement MovieList component here
+import React from 'react';
+import propTypes from 'prop-types';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+    render() {
+        const { movies } = this.props;
+
+        return (
+            movies.map((movie) => (
+                <MovieCard movie={movie} key={movie.title} />))
+
+        )
+    }
+}
+
+MovieList.propTypes = { movies: propTypes.arrayOf.isRequired };
+
+export default MovieList;
