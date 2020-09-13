5aa1d3d54456072a025554799a496c170a6ab932
Bruno Dantas <bruno.wesley77@gmail.com>
2020-09-13 12:37:10 -0300

    issue fix codeclimate

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,12 @@
 // implement Header component here
 
-import React from "react";
+import React from 'react';
 
 class Header extends React.Component {
   render() {
     return (
       <h1>Movie Cards Library</h1>
-    )
+    );
   }
 }
 
