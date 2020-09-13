d56b40206dd85437fddec0e422a8b58a87c48feb
Bruno Lemos <brunolemos.ssa@gmail.com>
2020-09-12 21:11:02 -0300

    Implementaçao do primeiro requisito

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,13 @@
-// implement Header component here.
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div>
+        <span>Header Component</span>
+      </div>
+    );
+  }
+}
+
+export default Header;
