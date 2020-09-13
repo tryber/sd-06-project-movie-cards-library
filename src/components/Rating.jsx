2333ed3f31c12846d141667b4726b4ed81a5e08d
Bruno Lemos <brunolemos.ssa@gmail.com>
2020-09-13 16:19:15 -0300

    Adicionados proptypes do componente Rating

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Rating extends React.Component {
   render() {
@@ -8,5 +9,9 @@ class Rating extends React.Component {
     );
   }
 }
+
+Rating.propTypes = {
+  ratingInfo: PropTypes.number,
+};
 
 export default Rating;
