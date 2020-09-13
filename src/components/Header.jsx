39c4e1f897bf26174d1094868798b2066c3ca346
VinicinBraga <vinicius@bragamatta.com.br>
2020-09-13 11:53:35 -0300

    Requisito Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ class Header extends React.Component {
   render() {
     return (
       <header className="page-head">
-          <h1 className="page-title">Movie Cards</h1>
+        <h1 className="page-title">Movie Cards</h1>
       </header>
     );
   }
