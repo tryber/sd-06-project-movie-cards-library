5986a1b909b174e1a19efa64de4c32319f23530e
Bruno Lemos <brunolemos.ssa@gmail.com>
2020-09-12 21:41:55 -0300

    Pequenas correçoes no arquivo MovieList.jsx

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -3,9 +3,7 @@ import React from 'react';
 class MovieList extends React.Component {
   render() {
     return (
-      <div>
-        <span>MovieList Component</span>
-      </div>
+      <span>MovieList Component</span>
     );
   }
 }
