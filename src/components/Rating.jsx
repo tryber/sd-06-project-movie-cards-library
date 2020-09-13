3e7e06655ad38828e950ed276984ae76fdb8cc09
Alexandre Fogaça <alexandr.fogaca@gmail.com>
2020-09-13 14:21:27 -0300

    Teste de requisitos

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,13 @@
-// implement Rating component here
+import React from 'react';
+
+class Rating extends React.Component {
+  render () {
+    return (
+      <span className='movie-card-rating'>
+        <p className='rating'>{this.props.rating}</p>
+      </span>
+    )
+  }
+}
+
+export default Rating;
