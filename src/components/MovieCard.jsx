4afab7de93a7b1d126b0bd540437cafc82d68aa2
Gabriel Luiz da Silva <gabrielsilva.adm@gmail.com>
2020-09-12 21:21:40 -0300

    tudo menos o rating

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,29 @@
-// implement MovieCard component here
+import React, { Component } from 'react';
+import propTypes from 'prop-types';
+
+class MovieCard extends Component{
+	render(){
+		const { title, subtitle, storyline, imagePath} = this.props.movie;
+
+		return (
+			<div className="movieCard">
+				<img src={imagePath} alt={title} />
+				<h4>{title}</h4>
+				<h5>{subtitle}</h5>
+				<p>{storyline}</p>
+
+			</div>
+			
+		);
+	}
+}
+
+MovieCard.propTypes = {
+	title: propTypes.string.isRequired,
+	subtitle: propTypes.string.isRequired,
+	storyLine: propTypes.string.isRequired,
+	imagePath: propTypes.string.isRequired,
+	
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,21 @@
-// implement MovieList component here
+import React, { Component } from 'react';
+import MovieCard from './MovieCard';
+import propTypes from 'prop-types';
+
+class MovieList extends Component{
+	render(){
+		const { movies } = this.props;
+		return (
+			<div>
+				{movies.map((movie) => (
+					<MovieCard movie={movie} key={movie.title} />
+
+				))}
+			</div>
+		)
+	}
+}
+
+MovieList.propTypes = {movies: propTypes.arrayOf(propTypes.object).isRequired};
+
+export default MovieList;
