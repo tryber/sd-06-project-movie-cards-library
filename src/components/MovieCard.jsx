2a570a64591518fe66d5c2188a7322087fcaa979
Bruno Lemos <brunolemos.ssa@gmail.com>
2020-09-13 15:38:23 -0300

    Aplicados estilos nos elementos do componente MovieCard

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -5,12 +5,16 @@ class MovieCard extends React.Component {
   render() {
     const { title, subtitle, storyline, imagePath, rating } = this.props.movie;
     return (
-      <div>
-        <img src={imagePath} alt={`${title} Movie Image`} />
-        <h4>{title}</h4>
-        <h5>{subtitle}</h5>
-        <p>{storyline}</p>
-        <Rating rating={rating} />
+      <div className="movie-card">
+        <div className="movie-card-header">
+          <img className="movie-card-image" src={imagePath} alt={`${title} Movie Image`} />
+        </div>
+        <div className="movie-card-body">
+          <h4 className="movie-card-title">{title}</h4>
+          <h5 className="movie-card-subtitle">{subtitle}</h5>
+          <p className="movie-card-storyline">{storyline}</p>
+          <Rating rating={rating} />
+        </div>
       </div>
     );
   }
