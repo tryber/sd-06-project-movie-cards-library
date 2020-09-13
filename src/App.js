0db26036f08895062c600f94cfba19cd9fb08a48
Ailson Lima Jr <ailsonjr21@gmail.com>
2020-09-13 15:55:45 -0300

    Corrigido erros apontados pelo codeclimate

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header.jsx'
+import Header from './components/Header.jsx';
 
 function App() {
   return (
     <div className="App">
-      < Header />
+      <Header />
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 
 class Header extends Component {
-  render (){
+  render() {
     return (
       <h1>Movie Card Library</h1>
-    )
+    );
   }
 }
 
