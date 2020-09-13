b1d73b28f109bdd5d4a71a400f0425fb0a2d2693
VinicinBraga <vinicius@bragamatta.com.br>
2020-09-13 11:51:10 -0300

    Requisito Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,13 @@
-// implement Header component here
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header className="page-head">
+          <h1 className="page-title">Movie Cards</h1>
+      </header>
+    );
+  }
+}
+
+export default Header;
