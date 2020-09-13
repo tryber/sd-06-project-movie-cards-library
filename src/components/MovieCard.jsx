489553befd8bd9ea9c690aa98c6d06f88df91683
VinicinBraga <vinicius@bragamatta.com.br>
2020-09-13 13:17:55 -0300

    Ate o requisito 10

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,17 @@
-// implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+    render() {
+        return (
+        <div className="movie-image">
+            <img className="movie-card-title" src={this.props.movie.imagePath} alt="Movie Cover" />
+            <h4 className="movie-card-title">{this.props.movie.title}</h4>
+            <h5 className="movie-card-subtitle">{this.props.movie.subtitle}</h5>
+            <p className="movie-card-storyline">{this.props.movie.storyline}</p>
+            </div>
+          );
+        }
+      }
+      
+MovieCard.defaultProps = { movie: {} };
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,15 @@
-// implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+      const movies = this.props.movies;
+      return (
+      <section className="movie-list">
+          {movies.map((currentsmovie) => <MovieCard key={currentsmovie.title} movie={currentsmovie} />)};
+      </section>);
+  }
+}
+
+MovieList.defaultProps = { movies: [] };
+export default MovieList;
