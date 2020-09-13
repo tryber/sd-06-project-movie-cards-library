5d968b6d4ae6b6be4b3726fc4e9849e31bb82634
Bruno Lemos <brunolemos.ssa@gmail.com>
2020-09-12 22:50:27 -0300

    Correçoes no arquivo MovieList.jsx

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
-    const movies = this.props.movies;
+    const moviesList = this.props.movies;
     return (
-      movies.map((movie) => (
+      moviesList.map((movie) => (
         <MovieCard />
       ))
     );
   }
 }
+
+MovieList.propTypes = {
+  moviesList: PropTypes.object,
+};
 
 export default MovieList;
