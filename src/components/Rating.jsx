1ee284f5b607d6a8c0c7cbe86103d16eb0862cc9
Jean Machado <jeanvictor.arq@gmail.com>
2020-09-13 15:35:57 -0300

    corrigindo codeclimate 3

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -5,10 +5,10 @@ class Rating extends React.Component {
   render() {
     const { rating } = this.props;
 
-	return <div className="rating">{rating}</div>;
+    return <div className="rating">{rating}</div>;
   }
 }
 
-Rating.propTypes = { rating: PropTypes.number.isRequired, };
+Rating.propTypes = { rating: PropTypes.number.isRequired, }
 
 export default Rating;
