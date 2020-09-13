3a7eb71284a8783f98918a08a33c9b65b9e2b169
William Gomes <williamjog@hotmail.com>
2020-09-13 02:26:24 -0300

    Improving CSS style

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -12,6 +12,8 @@ class MovieCard extends React.Component {
           <h4 className=".movie-card-title">{movie.title}</h4>
           <h5 className="movie-card-subtitle">{movie.subtitle}</h5>
           <p className="movie-card-storyline">{movie.storyline}</p>
+        </div>
+        <div>
           <Rating rating={movie.rating} />
         </div>
       </div>
