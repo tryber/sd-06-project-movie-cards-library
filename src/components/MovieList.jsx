50ed74f3caf26f6c16e2b11a54d02d0b2f187e10
Bruno Lemos <brunolemos.ssa@gmail.com>
2020-09-12 21:32:22 -0300

    Requisito 3 - Implementado o componente MovieList

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,13 @@
-// implement MovieList component here
+import React from 'react';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      <div>
+        <span>MovieList Component</span>
+      </div>
+    );
+  }
+}
+
+export default MovieList;
