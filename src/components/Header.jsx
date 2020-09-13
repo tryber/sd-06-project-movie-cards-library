77b44f15bd59766e2744d69ac173c82e02d8448e
STEFANO-BRANZ <stefano.branz@gmail.com>
2020-09-13 18:12:07 -0300

    Added CSS from project

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,8 +4,8 @@ class Header extends Component {
   render() {
     // Retorno meu header da forma como quero e exporto para ser importado pelo 'app.js'
     return (
-      <header>
-        <h1 className="title">Movie Cards Library</h1>
+      <header className="movie-card-header">
+        <h1 className="page-title">Movie Cards Library</h1>
       </header>
     );
   }
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -6,12 +6,16 @@ class MovieCard extends Component {
   render() {
     const { title, subtitle, storyline, imagePath, rating } = this.props.movie;
     return (
-      <div>
-        <img src={imagePath} alt={title} />
-        <h4>{ title }</h4>
-        <h5>{ subtitle }</h5>
-        <p>{ storyline }</p>
-        <div><Rating rating={rating} /></div>
+      <div className="movie-card">
+        <div className="movie-card-body">
+          <img className="movie-card-image" src={imagePath} alt={title} />
+          <h4 className="movie-card-title">{ title }</h4>
+          <h5 className="movie-card-subtitle">{ subtitle }</h5>
+          <p className="movie-card-storyline">{ storyline }</p>
+          <div className="movie-card-rating">
+            <Rating rating={rating} />
+          </div>
+        </div>
       </div>
     );
   }
