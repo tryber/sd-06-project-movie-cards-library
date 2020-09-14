2e333e57f4df085e75bffeba0c191a1bc43c1369
William Gomes <williamjog@hotmail.com>
2020-09-13 21:39:40 -0300

    Fixing | Failed prop type: The prop rating is marked as required in Rating, but its value is undefined

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -6,12 +6,14 @@ class Rating extends React.Component {
     const { rating } = this.props;
     return (
       <div className="movie-card-rating">
+        <label className="label">Rating:</label>
         <span className="rating">{rating}</span>
       </div>
     );
   }
 }
 
-Rating.propTypes = { rating: PropTypes.number.isRequired };
+
+Rating.propTypes = PropTypes.number.isRequired;
 
 export default Rating;
