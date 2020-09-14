9ab480a6b961ad10e6dcdaee028cc26de922dc98
Pedro Duarte <ikbenpedro@gmail.com>
2020-09-14 10:23:42 -0300

    cc 1.2

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class Rating extends React.Component {
-  render() {
-			const { rating } = this.props;
-			return (
-				<div className="movie-card-rating">
-							<span className="rating">{rating}</span>
-					</div>	
+	render() {
+		const { rating } = this.props;
+		return (
+			<div className="movie-card-rating">
+				<span className="rating">{rating}</span>
+			</div>
 		);
 	}
 }
