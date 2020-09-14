d43ed84cb00d5ab03c91f163dcc95e306e09299f
Anderson R <misterregisslv@gmail.com>
2020-09-13 22:51:53 -0300

    commit inicial do projeto cards-library allez

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,23 @@
-// implement MovieCard component here
+import React, { Component } from 'react';
+// import propTypes from 'prop-types';
+import Rating from './Rating';
+
+class MovieCard extends Component {
+  render() {
+    const movie = this.props;
+    const { imagePath, title, subtitle, storyline, rating } = movie.movie;
+    return (
+      <div className="movie-card">
+        <div className="movie-card-body">
+          <img className="movie-card-image" src={imagePath} alt={`Imagem do Filme${title}`} height="160px" />
+          <h4 className="movie-card-title">{title}</h4>
+          <h5 className="movie-card-subtitle">{subtitle}</h5>
+          <p className="movie-card-storyline">{storyline}</p>
+          <Rating rating={rating} />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,15 @@
-// implement MovieList component here
+import React, { Component } from 'react';
+import movies from '../data';
+import MovieCard from './MovieCard';
+
+class MovieList extends Component {
+  render() {
+    return (
+      <div className="movie-list">
+        {movies.map((mov) => <MovieCard key={mov.title}movie={mov} />)}
+      </div>
+    );
+  }
+}
+
+export default MovieList;
