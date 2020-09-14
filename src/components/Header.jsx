923a47682249ff78db65a9629545dcadff02e8f4
Bruno Dantas <bruno.wesley77@gmail.com>
2020-09-14 10:59:12 -0300

    renders without crashing

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
 // implement Header component here
-
 import React from 'react';
 
 class Header extends React.Component {
   render() {
     return (
-      <h1>Movie Cards Library</h1>
+      <div>
+        <h1>Movie Cards Library</h1>
+      </div>
     );
   }
 }
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,15 @@
 // implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    return(
+      <div className='MovieCard'>
+
+      </div>
+    ); 
+  }
+}
+
+export default MovieList;
