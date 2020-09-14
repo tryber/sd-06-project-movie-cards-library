82705168e3fe24e89eb9da20f508076998815842
Rebeca Cerqueira <rcks.santos@gmail.com>
2020-09-14 10:19:33 -0300

    cria o componente MovieList, sem qualquer relacionamento com o banco de dados. Testa com sucesso se o componente é exibido na tela.

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,13 @@
-// implement MovieList component here
+import React, { Component } from 'react';
+import movies from '../data'
+
+class MovieList extends Component {
+  render() {
+    return (<div>
+      <p>teste</p>
+    </div>
+      )
+  }
+}
+
+export default MovieList;
