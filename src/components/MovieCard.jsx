64d6be17f92a98ed3f15bbbea75e527be5a837bd
Rebeca Cerqueira <rcks.santos@gmail.com>
2020-09-14 11:52:21 -0300

    atribui aos cards suas respectivas tags, como solicitado além disso, insere as classes CSS para estilização da página

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -6,11 +6,14 @@ class MovieCard extends Component {
   render() {
     const { title, subtitle, storyline, imagePath, rating } = this.props;
     return (
-      <div>
-        <p>{title} e {subtitle} e</p>
-        <p>{storyline}</p>
-        <p>{imagePath}</p>
-        <Rating rating={rating} />
+      <div className="movie-card">
+        <img className="movie-card-image" src={imagePath} alt="imagem do filme" />
+        <div className="movie-card-body">
+          <h4 className="movie-card-title">{title}</h4>
+          <h5 className="movie-card-subtitle">{subtitle}</h5>
+          <p className="movie-card-storyline">{storyline}</p>
+          <Rating rating={rating} />
+        </div>
       </div>
     );
   }
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -4,7 +4,7 @@ import MovieCard from './MovieCard';
 
 class MovieList extends Component {
   render() {
-    return (<div>
+    return (<div className="movie-list">
       <MovieCard
         title={movies[0].title}
         subtitle={movies[0].subtitle}
