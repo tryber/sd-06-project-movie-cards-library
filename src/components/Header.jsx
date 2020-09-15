93a5505f1dd0cb1a4427e63f8d559ab0e68e3148
Fabio Camargos <fabiocmmcm@gmail.com>
2020-09-15 15:20:05 -0300

    movie card and 1 required ok

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 class Header extends React.Component {
     render(){
         return(
-            <div className='movie-card-header'>
+            <header className="movie-card-header">
                 <h1>Movie Cards Library</h1>
-            </div>
+            </header>
         );
     }
 }
