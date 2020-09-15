461a897653797cffef8d44e83a4ec6e26483034f
Elisa <elisalibanio1@gmail.comgit config --global user.email elisalibanio1@gmail.com>
2020-09-15 20:37:25 -0300

    enviando com 0 issues no codeclimate

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,13 @@
-// implement Rating component here
+import React from 'react';
+import PropTypes from 'prop-types';
+import '../App.css';
+
+
+class Rating extends React.Component {
+  render() {
+    return <span className="rating">{this.props.rating}</span>;
+  }
+}
+
+Rating.propTypes = { rating: PropTypes.number };
+export default Rating;
