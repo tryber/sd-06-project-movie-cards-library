a1ca4c66185a8925542ba2425fb5f067e863be5c
ilanribeiro <ilanribeiro@gmail.com>
2020-09-16 08:20:27 -0300

    Header element implemented and render without crash

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Header extends Component {
   render() {
     return (
-      <header className='movie-card-header page-title'>Movie Cards</header>
+      <header className='movie-card-header page-title'>Movie Cards Library</header>
     )
   }
 }
