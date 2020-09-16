b7aab80c42b5740c29943ea0fdcac1b600ab562c
ilanribeiro <ilanribeiro@gmail.com>
2020-09-16 20:23:10 -0300

    add css movie-card-title

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -8,7 +8,7 @@ class MovieCard extends Component {
     return (
       <div className='movie-card movie-card-body'>
         <img src={imagePath} alt='Movie Avatar' className='movie-card-image' />
-        <h4>{title}</h4>
+        <h4 className='movie-card-title'>{title}</h4>
         <h5>{subtitle}</h5>
         <p>{storyline}</p>
         <Rating rating={rating} />
