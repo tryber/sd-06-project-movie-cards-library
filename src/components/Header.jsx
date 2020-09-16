ee7a94776a845a63d4771e34946ac45f16682b49
ilanribeiro <ilanribeiro@gmail.com>
2020-09-16 11:58:47 -0300

    <MovieList /> render without crash and tag "h1" with text "Movie Cards Library"

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,9 @@ import React, { Component } from 'react';
 class Header extends Component {
   render() {
     return (
-      <header className='movie-card-header page-title'>Movie Cards Library</header>
+      <header className='movie-card-header page-title'>
+        <h1>Movie Cards Library</h1>
+      </header>
     )
   }
 }
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,11 @@
-// implement MovieList component here
+import React, { Component } from 'react';
+
+class MovieList extends Component {
+  render() {
+    return (
+      <div></div>
+    )
+  }
+}
+
+export default MovieList;
