058450964d65ebf36665d4ed8074b749c7845272
Elisa <elisalibanio1@gmail.comgit config --global user.email elisalibanio1@gmail.com>
2020-09-15 21:20:40 -0300

    agoravai

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -5,7 +5,7 @@ import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
-     const { movie } = this.props;
+    const { movie } = this.props;
     const { title, subtitle, storyline, imagePath, rating } = movie;
     return (
       <div className="movie-card">
