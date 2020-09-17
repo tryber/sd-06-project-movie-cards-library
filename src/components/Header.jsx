5a42be0e07ca49acf34c50609bc3ddf44d4fe18c
William Dieter <williamzdieter@gmail.com>
2020-09-17 16:52:17 -0300

    Add Req 1 e 2 - Passaram nos testes locais

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,2 +1,9 @@
-// implement Header component here
-//teste
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return <header><h1 className="header">Movie Cards Library</h1></header>;
+  }
+}
+
+export default Header;
