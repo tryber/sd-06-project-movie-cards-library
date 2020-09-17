779e6b4334ab2b869764f1b950ad1ecbb03278df
ilanribeiro <ilanribeiro@gmail.com>
2020-09-16 23:41:10 -0300

    update codeclimate in the file MovieCard.jsx

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,20 +1,32 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Rating from './Rating';
+
 
 class MovieCard extends Component {
   render() {
     const { title, subtitle, storyline, imagePath, rating } = this.props.movie;
 
     return (
-      <div className='movie-card movie-card-body'>
-        <img src={imagePath} alt='Movie Avatar' className='movie-card-image' />
-        <h4 className='movie-card-title'>{title}</h4>
-        <h5 className='movie-card-subtitle'>{subtitle}</h5>
-        <p className='movie-card-storyline'>{storyline}</p>
+      <div className="movie-card movie-card-body">
+        <img src={imagePath} alt="Movie Avatar" className="movie-card-image" />
+        <h4 className="movie-card-title">{title}</h4>
+        <h5 className="movie-card-subtitle">{subtitle}</h5>
+        <p className="movie-card-storyline">{storyline}</p>
         <Rating rating={rating} />
       </div>
-    )
+    );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape([{
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+    storyline: PropTypes.string.isRequired,
+    imagePath: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
+  }]).isRequired,
+};
 
 export default MovieCard;
