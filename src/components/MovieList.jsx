325293ffa69dbb997493b9829765268e8b4909c5
ilanribeiro <ilanribeiro@gmail.com>
2020-09-17 01:04:08 -0300

    correct codeclimate  issues about propTypes in the MovieList file

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import MovieCard from './MovieCard';
-
 
 class MovieList extends Component {
   render() {
@@ -13,5 +13,15 @@ class MovieList extends Component {
     );
   }
 }
+
+MovieList.propTypes = {
+  movies: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+    storyline: PropTypes.string.isRequired,
+    imagePath: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
+  }).isRequired,
+};
 
 export default MovieList;
