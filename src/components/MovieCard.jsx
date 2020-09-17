e5d8eb4a5f3fc157e16c7ca9b9415a5826fd895d
Kesley Lahoz <namielahoz@gmail.com>
2020-09-17 10:35:58 -0300

    fixing CC issues 2

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,30 +1,30 @@
-import React from "react";
-import PropTypes from "prop-types";
-import Rating from "./Rating";
+import React from 'react';
+import PropTypes from 'prop-types';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
     const { title, subtitle, storyline, imagePath, rating } = this.props.movie;
-      return (
-          <div className="movie-card-body">
-             <div className="movie-card-image">
-              <img src={ imagePath } alt={ "Movie Photo" } />
-             </div>
-             <div className="movie-card-title">
-               <h4>{ title }</h4>
-             </div>
-             <div className="movie-card-subtitle">
-               <h5>{ subtitle }</h5>
-             </div>
-             <div className="movie-card-storyline">
-               <p>{ storyline }</p>
-             </div>
-             <div className="movie-card-rating">
-               <Rating rating={ rating } />
-             </div>
-          </div>
-        );
-    }
+    return (
+      <div className="movie-card-body">
+        <div className="movie-card-image">
+          <img src={imagePath} alt="string" />
+        </div>
+        <div className="movie-card-title">
+          <h4>{ title }</h4>
+        </div>
+        <div className="movie-card-subtitle">
+          <h5>{ subtitle }</h5>
+        </div>
+        <div className="movie-card-storyline">
+          <p>{ storyline }</p>
+        </div>
+        <div className="movie-card-rating">
+          <Rating rating={rating} />
+        </div>
+      </div>
+    );
+  }
 }
 
 MovieCard.propTypes = {
