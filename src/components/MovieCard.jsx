6fe06cb5a7875490fab0be330fa02a7bd9df0971
vbergadev <vbergadev@gmail.com>
2020-09-18 12:23:39 -0300

    1parte

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,14 @@
-// implement MovieCard component here
+import React from 'react'
+
+class MovieCard extends React.Component {
+    render() {
+        return   <div>
+        <p>{this.props.title}</p>
+        <p>{this.props.subtitle}</p>
+        <p>{this.props.storyline}</p>
+        <p>{this.props.rating}</p>
+        <img src={this.props.imagePath} alt={this.props.title}/>
+      </div>
+    }
+}
+export default MovieCard
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,17 @@
-// implement MovieList component here
+import React from 'react'
+import movies from '../data'
+import MovieCard from './MovieCard'
+
+class MovieList extends React.Component {
+  render(){
+    return (
+      <div>
+      {movies.map((movie,index) =>
+       <MovieCard key={index} title={movie.title} subtitle={movie.subtitle} storyline={movie.storyline} rating={movie.rating} imagePath={movie.imagePath}/>)
+      }
+      </div>
+    )
+    }
+}
+
+export default MovieList
