b1e73d3cb5fee7314d19ee382b275e47508a531c
Anderson R <misterregisslv@gmail.com>
2020-09-18 01:45:27 -0300

    commit de atualização

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
-import movies from '../data';
 import MovieCard from './MovieCard';
+import PropTypes from 'prop-types';
 
 class MovieList extends Component {
   render() {
     return (
       <div className="movie-list">
-        {movies.map((mov) => <MovieCard key={mov.title}movie={mov} />)}
+        {this.props.movies.map((mov) => <MovieCard key={mov.title}movie={mov} />)}
       </div>
     );
   }
 }
 
 export default MovieList;
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object.isRequired),
+}
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Rating extends Component {
   render() {
@@ -14,3 +15,7 @@ class Rating extends Component {
 }
 
 export default Rating;
+
+Rating.propTypes = {
+  rating: PropTypes.number.isRequired,
+}
