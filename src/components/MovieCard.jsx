d0a85c6c1f01d9f771d181311abf04aaa0bd7dd5
Paulo Frazão <paulo4187@gmail.com>
2020-09-25 12:57:07 -0300

    alteração Rendereização de MovieList -< Rating rating={}>

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -14,7 +14,7 @@ class MovieCard extends React.Component {
           <h4 className="movie-card-title">{title}</h4>
           <h5 className="movie-card-subtitle">{subtitle}</h5>
           <p className="movie-card-storyline">{storyline}</p>
-          <div className="movie-card-rating"><Rating rating={rating} /></div>
+          <Rating rating={rating} />
         </div>
       </div>
     );
