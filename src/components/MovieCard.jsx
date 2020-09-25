9cb9573da38deb0766eefaa493d09ce6ca3770af
Paulo Frazão <paulo4187@gmail.com>
2020-09-25 10:32:18 -0300

    codeC

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -20,13 +20,13 @@ class MovieCard extends React.Component {
 }
 
 MovieCard.propTypes = ({
-  movie: {
+  movie: {  
     title: PropTypes.string.isRequired,
     subtitle: PropTypes.string.isRequired,
     storyline: PropTypes.string.isRequired,
     imagePath: PropTypes.string.isRequired,
     rating: PropTypes.number.isRequired,
-  }
+  }.isRequired,
 });
 
 export default MovieCard;
