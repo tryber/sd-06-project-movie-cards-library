82b297933cc1ff7cfc81059882b86b1af4643f7b
Paulo Frazão <paulo4187@gmail.com>
2020-09-25 10:26:38 -0300

    codeC

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -19,14 +19,14 @@ class MovieCard extends React.Component {
   }
 }
 
-// MovieCard.propTypes = ({
-//   movie: {
-//     title: PropTypes.string.isRequired,
-//     subtitle: PropTypes.string.isRequired,
-//     storyline: PropTypes.string.isRequired,
-//     imagePath: PropTypes.string.isRequired,
-//     rating: PropTypes.number.isRequired,
-//   }
-// });
+MovieCard.propTypes = ({
+  movie: {
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+    storyline: PropTypes.string.isRequired,
+    imagePath: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
+  }
+});
 
 export default MovieCard;
